025f332fa5b015a16597e532af437af651195807
loren <loren@a99b2113-df67-db11-8bcc-000c76553aea>
2007-03-01 03:47:02 +0000

    List all methods
    git-svn-id: http://soen.ca/svn/projects/ruby/yard/trunk@245 a99b2113-df67-db11-8bcc-000c76553aea

--- test_generate_html.rb
+++ test_generate_html.rb
@@ -9,12 +9,32 @@ ac.puts <<-eof
     <base target="main" />
   </head>
   <body>
+  <h3>All Classes</h3>
 eof
+meths = []
 Namespace.all.sort.each do |path|
   object = Namespace.at(path)
+  if object.is_a? MethodObject
+    meths << [object.name, object]
+  end
+  
   next unless object.is_a? ClassObject
   ac.puts "<a href='" + path.gsub("::","_") + ".html'>" + path + "</a><br />"
   File.open("doc/#{path.gsub('::','_')}.html", "w") {|f| f.write(object.format) }
 end
 ac.puts "</body></html>"
 ac.close
+
+File.open("doc/all-methods.html", "w") do |f|
+  f.puts <<-eof
+    <html>
+      <head>
+        <base target="main" />
+      </head>
+      <body>
+      <h3>All Methods</h3>
+eof
+  meths.sort {|a,b| a.first <=> b.first }.each do |name, object|
+    f.puts "<a href='" + object.parent.path.gsub("::", "_") + ".html##{object.scope}_method-#{name}'>#{name}</a><br />"
+  end
+end
