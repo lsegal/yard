66029fc308ed2ced8f7d469aaadb551c7f6c76a9
Loren Segal <lsegal@soen.ca>
2008-02-27 00:27:45 -0500

    Deal with a list of includes in one line

--- lib/handlers/mixin_handler.rb
+++ lib/handlers/mixin_handler.rb
@@ -4,9 +4,10 @@ class YARD::MixinHandler < YARD::CodeObjectHandler
   def process
     return unless object.is_a? YARD::CodeObjectWithMethods
     begin
-      object.mixins.push eval("[ " + statement.tokens[1..-1].to_s + " ]").to_s
+      # Verify that it's a real list with no funny stuff
+      object.mixins.push *eval("[ " + statement.tokens[1..-1].to_s + " ]").to_s.split(",")
     rescue NameError
-      object.mixins.push statement.tokens[1..-1].to_s
+      object.mixins.push *statement.tokens[1..-1].to_s.split(",")
     rescue SyntaxError
       Logger.warning "Undocumentable included module #{statement.tokens[1..-1].to_s}"
     end
