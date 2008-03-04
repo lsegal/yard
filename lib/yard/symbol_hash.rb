6e710b8bf6a36c12d983369b346a3671ad992e1d
Loren Segal <lsegal@soen.ca>
2008-03-04 11:43:26 -0500

    Symbolize any strings as well

--- lib/yard/symbol_hash.rb
+++ lib/yard/symbol_hash.rb
@@ -1,5 +1,5 @@
 class SymbolHash < Hash
-  def []=(key, value) super(key.to_sym, value) end
+  def []=(key, value) super(key.to_sym, value.is_a?(String) ? value.to_sym : value) end
   def [](key) super(key.to_sym) end
   def delete(key) super(key.to_sym) end
 end
