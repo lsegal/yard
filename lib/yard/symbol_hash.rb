6d42c3847030ce3020c91b209ebebd93bb555a29
Loren Segal <lsegal@soen.ca>
2008-03-04 12:43:26 -0500

    Symbolize value if string and add SymbolHash specs

--- lib/yard/symbol_hash.rb
+++ lib/yard/symbol_hash.rb
@@ -2,4 +2,5 @@ class SymbolHash < Hash
   def []=(key, value) super(key.to_sym, value.is_a?(String) ? value.to_sym : value) end
   def [](key) super(key.to_sym) end
   def delete(key) super(key.to_sym) end
+  def has_key?(key) super(key.to_sym) end
 end
---
+++ spec/extra/symbol_hash_spec.rb
@@ -0,0 +1,49 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe SymbolHash do
+  
+  it "should allow access to keys as String or Symbol" do
+    h = SymbolHash.new(false)
+    h['test'] = true
+    h[:test].should == true
+    h['test'].should == true
+  end
+  
+  it "should #delete by key as String or Symbol" do
+    h = SymbolHash.new
+    h.keys.length.should == 0
+
+    h['test'] = true
+    h.keys.length.should == 1
+
+    h.delete(:test)
+    h.keys.length.should == 0
+
+    h[:test] = true
+    h.keys.length.should == 1
+
+    h.delete('test')
+    h.keys.length.should == 0
+  end
+  
+  it "should return same #has_key? for key as String or Symbol" do
+    h = SymbolHash.new
+    h[:test] = 1
+    h.has_key?(:test).should == true
+    h.has_key?('test').should == true
+  end
+  
+  it "should symbolize value if it is a String" do
+    class Substring < String; end
+      
+    h = SymbolHash.new
+    h['test'] = Substring.new("hello")
+    h['test'].should == :hello
+  end 
+  
+  it "should not symbolize value if it is not a String" do
+    h = SymbolHash.new
+    h['test'] = [1,2,3]
+    h['test'].should == [1,2,3]
+  end
+end
