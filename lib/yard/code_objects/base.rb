91e0db63ba1c1fd0508c483daa746ae38524c7fe
Loren Segal <lsegal@soen.ca>
2008-05-10 04:21:14 -0400

    Add tag and docstring methods to CodeObjects::Base for later

--- lib/yard/code_objects/base.rb
+++ lib/yard/code_objects/base.rb
@@ -89,6 +89,18 @@ module YARD
         end
       end
 
+      ##
+      # Attaches a docstring to a code oject by parsing the comments attached to the statement
+      # and filling the {#tags} and {#docstring} methods with the parsed information.
+      #
+      # @param [String, Array<String>] comments 
+      #   the comments attached to the code object to be parsed 
+      #   into a docstring and meta tags.
+      def docstring=(comments)
+        parse_comments(comments) if comments
+      end
+
+      ##
       # Default type is the lowercase class name without the "Object" suffix
       # 
       # Override this method to provide a custom object type 
@@ -122,10 +134,93 @@ module YARD
     
       alias_method :parent, :namespace
       alias_method :parent=, :namespace=
-      
+
+      ## 
+      # Convenience method to return the first tag
+      # object in the list of tag objects of that name
+      #
+      # Example:
+      #   doc = YARD::Documentation.new("@return zero when nil")
+      #   doc.tag("return").text  # => "zero when nil"
+      #
+      # @param [#to_s] name the tag name to return data for
+      # @return [BaseTag] the first tag in the list of {#tags}
+      def tag(name)
+        name = name.to_s
+        @tags.find {|tag| tag.tag_name == name }
+      end
+
+      ##
+      # Returns a list of tags specified by +name+ or all tags if +name+ is not specified.
+      #
+      # @param name the tag name to return data for, or nil for all tags
+      # @return [Array<BaseTag>] the list of tags by the specified tag name
+      def tags(name = nil)
+        return @tags if name.nil?
+        name = name.to_s
+        @tags.select {|tag| tag.tag_name == name }
+      end
+
+      ##
+      # Returns true if at least one tag by the name +name+ was declared
+      #
+      # @param [String] name the tag name to search for
+      # @return [Boolean] whether or not the tag +name+ was declared
+      def has_tag?(name)
+        name = name.to_s
+        @tags.any? {|tag| tag.tag_name == name }
+      end
+
       protected
     
       def sep; NAMESPACE_SEPARATOR end
+
+      private
+
+      ##
+      # Parses out comments split by newlines into a new code object
+      #
+      # @param [Array<String>, String] comments 
+      #   the newline delimited array of comments. If the comments
+      #   are passed as a String, they will be split by newlines. 
+      def parse_comments(comments)
+        return if comments.empty?
+        meta_match = /^\s*@(\S+)\s*(.*)/
+        comments = comments.split(/\r?\n/) if comments.is_a? String
+        @tags, @docstring = [], ""
+
+        indent, last_indent = comments.first[/^\s*/].length, 0
+        tag_name, tag_klass, tag_buf = nil, nil, ""
+
+        # Add an extra line to catch a meta directive on the last line
+        (comments+['']).each do |line|
+          indent = line[/^\s*/].length 
+
+          if (indent < last_indent && tag_name) || line == '' || line =~ meta_match
+            tag_method = "#{tag_name}_tag"
+            if tag_name && TagLibrary.respond_to?(tag_method)
+              @tags << TagLibrary.send(tag_method, tag_buf.squeeze(" ")) 
+            end
+            tag_name, tag_buf = nil, ''
+          end
+
+          # Found a meta tag
+          if line =~ meta_match
+            tag_name, tag_buf = $1, $2 
+          elsif indent >= last_indent && tag_name
+            # Extra data added to the tag on the next line
+            tag_buf << line
+          else
+            # Regular docstring text
+            @docstring << line << "\n" 
+          end
+
+          last_indent = indent
+        end
+
+        # Remove trailing/leading whitespace / newlines
+        @docstring.gsub!(/\A[\r\n\s]+|[\r\n\s]+\Z/, '')
+      end      
     end
   end
 end
