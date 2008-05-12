91e8f3058c60b76aaa6e39f30f9e3e8bc9168bf7
Loren Segal <lsegal@soen.ca>
2008-05-12 03:41:52 -0400

    Implement visibility specs

--- spec/handlers/visibility_handler_spec.rb
+++ spec/handlers/visibility_handler_spec.rb
@@ -1,7 +1,18 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe YARD::Handlers::VisibilityHandler do
-  it "should be able to set visibility to public" 
-  it "should be able to set visibility to private"
-  it "should be able to set visibility to protected"
+  before { parse_file :visibility_handler_001, __FILE__ }
+  
+  it "should be able to set visibility to public" do
+    Registry.at("Testing#pub").visibility.should == :public
+    Registry.at("Testing#pub2").visibility.should == :public
+  end
+  
+  it "should be able to set visibility to private" do
+    Registry.at("Testing#priv").visibility.should == :private
+  end
+  
+  it "should be able to set visibility to protected" do
+    Registry.at("Testing#prot").visibility.should == :protected
+  end
 end
