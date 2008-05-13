aa122d3927080597d454660ae8d6f6875202ca99
Loren Segal <lsegal@soen.ca>
2008-05-13 00:15:28 -0400

    Add attribute handler and specs

--- lib/yard/handlers/attribute_handler.rb
+++ lib/yard/handlers/attribute_handler.rb
@@ -1,3 +1,55 @@
 class YARD::Handlers::AttributeHandler < YARD::Handlers::Base
   handles /\Aattr(?:_(?:reader|writer|accessor))?(?:\s|\()/
+  
+  def process
+    return unless namespace.is_a?(ClassObject) # attributes only work from classes
+    
+    begin
+      attr_type   = statement.tokens.first.text.to_sym
+      symbols     = eval("[" + statement.tokens[1..-1].to_s + "]")
+      read, write = true, false
+    rescue SyntaxError
+      Logger.warning "in AttributeHandler: Undocumentable attribute statement: '#{statement.tokens.to_s}'\n" +
+                     "\tin file '#{parser.file}':#{statement.tokens.first.line_no}"
+      return
+    end
+    
+    # Change read/write based on attr_reader/writer/accessor
+    case attr_type
+    when :attr
+      # In the case of 'attr', the second parameter (if given) isn't a symbol.
+      write = symbols.pop if symbols.size == 2
+    when :attr_accessor
+      write = true
+    when :attr_reader
+      # change nothing
+    when :attr_writer
+      read, write = false, true
+    end
+
+    # Add all attributes
+    symbols.each do |name| 
+      # Show their methods as well
+      [name, "#{name}="].each do |meth|
+        next unless (meth.to_s.include?("=") ? write : read)
+        
+        MethodObject.new(namespace, meth, scope) do |o|
+          if meth.to_s.include? "="
+            src = "def #{meth}(value)"
+            full_src = "#{src}\n  @#{name} = value\nend"
+            doc = "Sets the attribute +#{name}+\n@param value the value to set the attribute +#{name}+ to."
+          else
+            src = "def #{meth}"
+            full_src = "#{src}\n  @#{name}\nend"
+            doc = "Returns the value of attribute +#{name}+"
+          end
+          o.source = full_src
+          o.docstring = statement.comments.to_s.empty? ? doc : statement.comments
+          o.file = parser.file
+        end
+      end
+
+      namespace.attributes.update name => { :read => read, :write => write }
+    end
+  end
 end
---
+++ spec/handlers/attribute_handler_spec.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe YARD::Handlers::AttributeHandler do
+  before { parse_file :attribute_handler_001, __FILE__ }
+  
+  def read_write(namespace, name, read, write)
+    rname, wname = namespace.to_s+"#"+name.to_s, namespace.to_s+"#"+name.to_s+"="
+    if read
+      Registry.at(rname).should be_instance_of(CodeObjects::MethodObject) 
+    else
+      Registry.at(rname).should == nil
+    end
+    
+    if write
+      Registry.at(wname).should be_kind_of(CodeObjects::MethodObject) 
+    else
+      Registry.at(wname).should == nil
+    end     
+    
+    attrs = Registry.at(namespace).attributes[name]
+    attrs[:read].should == read
+    attrs[:write].should == write
+  end
+  
+  it "should only parse attributes inside classes" do
+    Registry.at("A#x=").should == nil
+  end
+  
+  it "should parse 'attr'" do
+    read_write(:B, :a, true, true)
+    read_write(:B, :a2, true, false)
+    read_write(:B, "a3", true, false)
+  end
+  
+  it "should parse 'attr_reader'" do
+    read_write(:B, :b, true, false)
+  end
+  
+  it "should parse 'attr_writer'" do
+    read_write(:B, :e, false, true)
+  end
+  
+  it "should parse 'attr_accessor'" do
+    read_write(:B, :f, true, true)
+  end
+  
+  it "should parse a list of attributes" do
+    read_write(:B, :b, true, false)
+    read_write(:B, :c, true, false)
+    read_write(:B, :d, true, false)
+  end
+  
+  it "should have a default docstring if one is not supplied" do
+    Registry.at("B#f=").docstring.should_not be_empty
+  end
+  
+  it "should set the correct docstring if one is supplied" do
+    Registry.at("B#b").docstring.should == "Docstring"
+    Registry.at("B#c").docstring.should == "Docstring"
+    Registry.at("B#d").docstring.should == "Docstring"
+  end
+end
