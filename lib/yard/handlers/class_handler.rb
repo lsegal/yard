5f0a0e38951da35446afe64628a9fb1ed2e03dfd
Loren Segal <lsegal@soen.ca>
2008-05-18 05:34:57 -0400

    Allow class << ClassName

--- lib/yard/handlers/class_handler.rb
+++ lib/yard/handlers/class_handler.rb
@@ -14,10 +14,13 @@ class YARD::Handlers::ClassHandler < YARD::Handlers::Base
       end
       parse_block(:namespace => klass)
     elsif statement.tokens.to_s =~ /^class\s*<<\s*([\w\:]+)/
-      if $1 == "self"
+      classname = $1
+      if classname == "self"
         parse_block(:namespace => namespace, :scope => :class)
+      elsif classname[0,1] =~ /[A-Z]/
+        parse_block(:namespace => P(namespace, classname), :scope => :class)
       else
-        YARD.logger.warn "in ClassHandler: Undocumentable class: '#{$1}'\n"
+        YARD.logger.warn "in ClassHandler: Undocumentable class: '#{klass}'\n"
                     "\tin file '#{parser.file}':#{statement.tokens.first.line_no}"
         return 
       end
--- spec/handlers/class_handler_spec.rb
+++ spec/handlers/class_handler_spec.rb
@@ -4,23 +4,27 @@ describe YARD::Handlers::ClassHandler do
   before { parse_file :class_handler_001, __FILE__ }
   
   it "should parse a class block with docstring" do
-    Registry.at("A").docstring.should == "Docstring"
+    P("A").docstring.should == "Docstring"
   end
   
   it "should handle complex class names" do
-    Registry.at("A::B::C").should_not == nil
+    P("A::B::C").should_not == nil
   end
   
   it "should handle the subclassing syntax" do
-    Registry.at("A::B::C").superclass.path.should == "String"
-    Registry.at("A::X").superclass.should == Registry.at("A::B::C")
+    P("A::B::C").superclass.path.should == "String"
+    P("A::X").superclass.should == Registry.at("A::B::C")
   end
   
   it "should interpret class << self as a class level block" do
-    Registry.at("A::classmethod1").should_not == nil
+    P("A::classmethod1").should_not == nil
+  end
+  
+  it "should interpret class << ClassName as a class level block in ClassName's namespace" do
+    P("A::B::C::Hello").should be_instance_of(CodeObjects::MethodObject)
   end
   
   it "should make visibility public when parsing a block" do
-    Registry.at("A::B::C#method1").visibility.should == :public
+    P("A::B::C#method1").visibility.should == :public
   end
 end
