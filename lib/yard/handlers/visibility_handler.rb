1b4f5468e7f54970ca4f7bdf6373da44400e8256
Loren Segal <lsegal@soen.ca>
2008-05-18 07:13:09 -0400

    Add support for private :methname to visibility handler

--- lib/yard/handlers/visibility_handler.rb
+++ lib/yard/handlers/visibility_handler.rb
@@ -1,7 +1,28 @@
 class YARD::Handlers::VisibilityHandler < YARD::Handlers::Base
-  handles /\A(protected|private|public)\Z/
+  handles /\A(protected|private|public)/
   
   def process
-    self.visibility = statement.tokens.to_s.to_sym
+    if statement.tokens.size == 1
+      self.visibility = statement.tokens.to_s.to_sym
+    else
+      last_tk = nil
+      vis = statement.tokens.first.text
+      statement.tokens[2..-1].each do |tk|
+        name = nil
+        if tk.is_a?(TkSTRING)
+          name = eval(tk.text)
+        elsif tk.is_a?(TkIDENTIFIER) && last_tk.is_a?(TkSYMBEG)
+          name = eval(":" + tk.text)
+        end
+        
+        if name
+          MethodObject.new(namespace, name, scope) do |obj|
+            obj.visibility = vis
+          end
+        end
+        
+        last_tk = tk
+      end
+    end
   end
 end
