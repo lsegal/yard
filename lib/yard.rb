20acde161b7ebe19a46eaa1eafa56ee6625ffbf7
Loren Segal <lsegal@soen.ca>
2008-05-18 05:29:25 -0400

    Add autoloading code, finally

--- lib/yard.rb
+++ lib/yard.rb
@@ -1,45 +1,12 @@
-YARD_ROOT = File.join(File.dirname(__FILE__), 'yard')
-YARD_TEMPLATE_ROOT = File.join(File.dirname(__FILE__), '..', 'templates')
-
-$LOAD_PATH.unshift(YARD_ROOT)
-
-['yard_logger'].each do |file|
-  require File.join(YARD_ROOT, file)
-end
-
 module YARD
-  VERSION = "0.2.1"
-  
-  module CodeObjects; end
-  module Generators; end
-  module Parser
-    module Lexer; end
-    module Ruby; end
-  end
-  module Handlers; end
-  module Tags
-    module Library; end
-  end
+  VERSION = "0.2.2"
+  ROOT = File.join(File.dirname(__FILE__), 'yard')
+  TEMPLATE_ROOT = File.join(File.dirname(__FILE__), '..', 'templates')
 end
 
-%w[ 
-  symbol_hash
-  tags/*
-  code_objects/base 
-  code_objects/namespace_object
-  code_objects/*
-  parser/**/*
-  handlers/base
-  handlers/*
-  generators/*
-  serializers/*
-  registry
-  tag_library
-].each do |file|
-  file = File.join(File.dirname(__FILE__), 'yard', file + ".rb")
-  Dir[file].each do |f|
-    if require(f)
-      YARD.logger.debug "Loading #{f}..."
-    end
-  end
+$:.unshift(YARD::ROOT)
+
+['logging', 'symbol_hash', 'autoload'].each do |file|
+  require file
 end
+
