ea47d746dac142e8f0e03ba08fe21b60da6f8695
Loren Segal <lsegal@soen.ca>
2008-05-23 14:03:40 -0400

    Expand specs on section handling

--- spec/generators/base_spec.rb
+++ spec/generators/base_spec.rb
@@ -4,20 +4,37 @@ require 'stringio'
 include YARD::Generators
 
 describe YARD::Generators::Base, 'Section handling' do
+  before { Registry.clear }
+  
   it "should allow a list of sections to be returned by sections_for" do
+    base = Generators::Base.new
+    base.stub!(:sections_for).and_return([:meth1, :meth2, :meth3])
+    base.should_receive(:meth1).and_return('a')
+    base.should_receive(:meth2).and_return('b')
+    base.should_receive(:meth3).and_return('c')
+    base.generate(Registry.root).should == 'abc'
+  end
+  
+  it "should allow a heirarchical list of sections to be returned by sections_for" do
+    base = Generators::Base.new
+    base.stub!(:sections_for).and_return([:meth1, [:meth2, :meth3]])
+    base.should_receive(:meth1).and_return('a')
+    base.should_not_receive(:meth2)
+    base.should_not_receive(:meth3)
+    base.generate(Registry.root).should == 'a'
+  end    
+  
+  it "should yield sub section lists to the parent section" do
     class XYZ < Generators::Base
       def sections_for(object) [:meth1, [:submeth1, :submeth2, [:submeth1]]] end
       def meth1(object) object.name.to_s + yield(object) end
       def submeth1(object) object.name.to_s end
       def submeth2(object) object.name.to_s + yield(P(:YARD)) end
     end
-    
+
     CodeObjects::Base.new(:root, :YARD)
     XYZ.new.generate(Registry.root).should == "rootrootrootYARD"
   end
-  
-  it "should allow a heirarchical list of sections to be returned by sections_for"
-  it "should yield sub section lists to the parent section"
 end
 
 describe YARD::Generators::Base, 'Rendering' do
