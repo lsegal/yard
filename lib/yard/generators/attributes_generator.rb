63375cd75044109904c9adc8dc75695d7c7ada7f
Loren Segal <lsegal@soen.ca>
2008-05-24 22:41:28 -0400

    Only show attributes section if there are attributes in the class

--- lib/yard/generators/attributes_generator.rb
+++ lib/yard/generators/attributes_generator.rb
@@ -1,6 +1,11 @@
 module YARD
   module Generators
     class AttributesGenerator < Base
+      def before_section(object)
+        size = object.class_attributes.size + object.instance_attributes.size
+        size > 0 ? super : false
+      end
+      
       def sections_for(object) [:header] end
     end
   end
