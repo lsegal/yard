cc44f531dc1bbafd1ae0b876a2e82de6569402fa
Loren Segal <lsegal@soen.ca>
2008-05-27 06:08:49 -0400

    Move method formatting helpers into MethodHelper

--- lib/yard/generators/method_signature_generator.rb
+++ lib/yard/generators/method_signature_generator.rb
@@ -1,35 +1,18 @@
 module YARD
   module Generators
     class MethodSignatureGenerator < Base
+      include Helpers::MethodHelper
+
+      before_generate :has_signature?
+      
       def sections_for(object) 
-        [:main] if object.signature
+        [:main] 
       end
       
       protected
       
-      def format_def(object)
-        object.signature.gsub(/^def\s*(?:.+?(?:\.|::)\s*)?/, '')
-      end
-      
-      def format_return_types(object)
-        typenames = "Object"
-        if object.has_tag?(:return)
-          types = object.tags(:return).map do |t| 
-            t.types.map do |type| 
-              type.gsub(/(^|[<>])\s*([^<>#]+)\s*(?=[<>]|$)/) {|m| $1 + linkify($2) }
-            end
-          end.flatten
-          typenames = types.size == 1 ? types.first : "[#{types.join(", ")}]"
-        end
-        typenames
-      end
-      
-      def format_block(object)
-        if object.has_tag?(:yieldparam)
-          "{|" + object.tags(:yieldparam).map {|t| t.name }.join(", ") + "| ... }"
-        else
-          ""
-        end
+      def has_signature?(object)
+        object.signature ? true : false
       end
     end
   end
