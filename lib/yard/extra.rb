df95afdea39c15d58a1566cb5acd8d000d27f485
Loren Segal <lsegal@soen.ca>
2008-05-28 23:27:55 -0400

    Update relative_path and add documentation

--- lib/yard/extra.rb
+++ lib/yard/extra.rb
@@ -1,12 +1,26 @@
 class File
+  RELATIVE_PARENTDIR = '..'
+  
+  # Turns a path +to+ into a relative path from starting
+  # point +from+. The argument +from+ is assumed to be
+  # a filename. To treat it as a directory, make sure it
+  # ends in {File::SEPARATOR} ('/' on UNIX filesystems).
+  # 
+  # @param [String] from the starting filename 
+  #   (or directory with +from_isdir+ set to +true+).
+  # 
+  # @param [String] to the final path that should be made relative.
+  # 
+  # @return [String] the relative path from +from+ to +to+.
+  # 
   def self.relative_path(from, to)
-    from = File.expand_path(from).split('/')
-    to = File.expand_path(to).split('/')
+    from = expand_path(from).split(SEPARATOR)
+    to = expand_path(to).split(SEPARATOR)
     from.length.times do 
       break if from[0] != to[0] 
       from.shift; to.shift
     end
     fname = from.pop
-    join *(from.map { '..' } + to)
+    join *(from.map { RELATIVE_PARENTDIR } + to)
   end
 end
