0273dfaa68cb58ed34251d1dca4dc4ce09c241c2
Loren Segal <lsegal@soen.ca>
2008-05-29 05:54:09 -0400

    Mock the parser

--- spec/handlers/spec_helper.rb
+++ spec/handlers/spec_helper.rb
@@ -3,6 +3,13 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 include Handlers
 
 def undoc_error(code)
-  c = self.class.described_type.new(nil, Parser::StatementList.new(code).first)
+  mock = mock("parser")
+  mock.stub!(:namespace).and_return(Registry.root)
+  mock.stub!(:owner).and_return(Registry.root)
+  mock.stub!(:scope).and_return(:instance)
+  mock.stub!(:visibility).and_return(:public)
+  mock.stub!(:file).and_return('<STDIN>')
+  
+  c = self.class.described_type.new(mock, Parser::StatementList.new(code).first)
   lambda { c.process }.should raise_error(UndocumentableError)
 end
