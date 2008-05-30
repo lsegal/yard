6e1563d11c9f671807ac0c1faec92e4086d66ce1
Loren Segal <lsegal@soen.ca>
2008-05-29 23:04:42 -0400

    Add formatting helpers from HtmlHelper

--- lib/yard/generators/helpers/base_helper.rb
+++ lib/yard/generators/helpers/base_helper.rb
@@ -1,5 +1,11 @@
 module YARD::Generators::Helpers
   module BaseHelper
+    # This is used a lot by the HtmlHelper and there should
+    # be some helper to "clean up" text for whatever, this is it.
+    def h(text)
+      text
+    end
+    
     def linkify(*args) 
       # The :// character sequence exists in no valid object path but just about every URL scheme.
       if args.first.is_a?(String) && args.first.include?("://")
@@ -24,6 +30,14 @@ module YARD::Generators::Helpers
     
     def link_url(url)
       url
+    end
+    
+    def format_object_name_list(objects)
+      objects.sort_by {|o| o.name.to_s.downcase }.join(", ")
+    end
+    
+    def format_types(list, brackets = true)
+      list.empty? ? "" : (brackets ? "[#{list.join(", ")}]" : list.join(", "))
     end
   end
 end
