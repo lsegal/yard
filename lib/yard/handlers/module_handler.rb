157420b619a3ac80cac483464d6216eb87f6ce9a
Loren Segal <lsegal@soen.ca>
2008-06-01 01:32:09 -0400

    Parse modules using NAMESPACEMATCH

--- lib/yard/handlers/module_handler.rb
+++ lib/yard/handlers/module_handler.rb
@@ -2,7 +2,7 @@ class YARD::Handlers::ModuleHandler < YARD::Handlers::Base
   handles TkMODULE
   
   def process
-    modname = statement.tokens.to_s[/^module\s+([^ ;]+)/, 1]
+    modname = statement.tokens.to_s[/^module\s+(#{NAMESPACEMATCH})/, 1]
     mod = ModuleObject.new(namespace, modname)
     parse_block(:namespace => mod)
     mod # return for registration
