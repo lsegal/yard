adbbaee765f38237424938332d544bda19cef859
Loren Segal <lsegal@soen.ca>
2008-06-01 04:34:52 -0400

    Update templates, stylesheets

--- lib/yard/generators/method_generator.rb
+++ lib/yard/generators/method_generator.rb
@@ -2,18 +2,29 @@ module YARD
   module Generators
     class MethodGenerator < Base
       before_generate :is_method?
+      before_section :aliases, :has_aliases?
       
       def sections_for(object) 
         [
           :header,
           [
+            :title,
+            [
+              G(MethodSignatureGenerator), 
+              :aliases
+            ], 
             G(DeprecatedGenerator), 
-            G(MethodSignatureGenerator), 
             G(DocstringGenerator), 
             G(TagsGenerator), 
             G(SourceGenerator)
           ]
         ]
+      end
+      
+      protected
+      
+      def has_aliases?(object)
+        !object.aliases.empty?
       end
     end
   end
