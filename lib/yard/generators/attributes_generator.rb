0fe6ce969c5c54d11af47bbacc76a1b85ccf95ee
Loren Segal <lsegal@soen.ca>
2008-06-01 23:46:39 -0400

    Update attributes generator to use uml in text mode

--- lib/yard/generators/attributes_generator.rb
+++ lib/yard/generators/attributes_generator.rb
@@ -4,10 +4,15 @@ module YARD
       include Helpers::MethodHelper
       
       before_generate :has_attributes?
+      before_list :includes
       
       def sections_for(object) [:header] end
         
       protected
+      
+      def includes
+        extend Helpers::UMLHelper if format == :text
+      end
       
       def has_attributes?
         current_object.class_attributes.size + current_object.instance_attributes.size > 0
