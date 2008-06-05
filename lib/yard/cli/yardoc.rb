54534620942303d10e4eb2a8e9c9bdf97a518f58
Loren Segal <lsegal@soen.ca>
2008-06-05 06:26:45 -0400

    Add template path, load file options for yardoc cli and default file loading to lib/**/*.rb

--- lib/yard/cli/yardoc.rb
+++ lib/yard/cli/yardoc.rb
@@ -21,6 +21,7 @@ module YARD
         @visibilities = [:public]
         @reload = true
         @generate = true
+        @files = ['lib/**/*.rb']
       end
       
       def run(*args)
@@ -38,51 +39,73 @@ module YARD
         serialopts = SymbolHash.new
         
         opts = OptionParser.new
-        opts.on('--use-cache', 
-                'Use the cached .yardoc database to generate documentation (defaults to no cache)') do 
+        opts.banner = "Usage: yardoc [options] [source files]"
+
+        opts.separator "(if a list of source files is omitted, lib/**/*.rb is used.)"
+        opts.separator ""
+        opts.separator "General Options:"
+
+        opts.on('-c', '--use-cache', 
+                'Use the cached .yardoc database to generate documentation. (defaults to no cache)') do 
           self.reload = false
         end
         
-        opts.on('--no-output', 'Only generate .yardoc database, no documentation.') do
+        opts.on('-n', '--no-output', 'Only generate .yardoc database, no documentation.') do
           self.generate = false
         end
         
-        opts.on('-d', '--output-dir [DIR]', 
-                'The output directory (defaults to ./doc)') do |dir|
-          serialopts[:basepath] = dir
+        opts.on('-e', '--load FILE', 'A Ruby script to load before the source tree is parsed.') do |file|
+          if !require(file.gsub(/\.rb$/, ''))
+            log.error "The file `#{file}' was already loaded, perhaps you need to specify the absolute path to avoid name collisions."
+            exit
+          end
+        end
+
+        opts.separator ""
+        opts.separator "Output options"
+  
+        opts.on('--no-public', "Don't show public methods. (default shows public)") do 
+          visibilities.delete(:public)
+        end
+
+        opts.on('--protected', "Show or don't show protected methods. (default hides protected)") do
+          visibilities.push(:protected)
+        end
+
+        opts.on('--private', "Show or don't show private methods. (default hides private)") do 
+          visibilities.push(:private) 
         end
         
-        opts.on('-r', '--readme [FILE]', 'The readme file used as the title page of documentation.') do |readme|
+        opts.on('-r', '--readme FILE', 'The readme file used as the title page of documentation.') do |readme|
           options[:readme] = readme
         end
         
-        opts.on('-t', '--template [TEMPLATE]', 
-                'The template to use (defaults to "default")') do |template|
+        opts.on('-d', '--output-dir PATH', 
+                'The output directory. (defaults to ./doc)') do |dir|
+          serialopts[:basepath] = dir
+        end
+
+        opts.on('-t', '--template TEMPLATE', 
+                'The template to use. (defaults to "default")') do |template|
           options[:template] = template.to_sym
         end
+
+        opts.on('-p', '--template-path PATH', 
+                'The template path to look for templates in. (used with -t).') do |path|
+          YARD::Generator::Base.register_template_path(path)
+        end
         
-        opts.on('-f', '--format [FORMAT]', 
-                'The output format for the template (defaults to html)') do |format|
+        opts.on('-f', '--format FORMAT', 
+                'The output format for the template. (defaults to html)') do |format|
           options[:format] = format
         end
-        
-        opts.on('--[no-]public', "Show or don't show public methods (default shows public)") do |value|
-          visibilities.send(value ? :push : :delete, :public) 
-        end
 
-        opts.on('--[no-]protected', "Show or don't show protected methods (default hides protected)") do |value|
-          visibilities.send(value ? :push : :delete, :protected) 
-        end
-
-        opts.on('--[no-]private', "Show or don't show private methods (default hides private)") do |value|
-          visibilities.send(value ? :push : :delete, :private) 
-        end
-        
-        opts.on('=[FILES]', 'files to parse')           { }
-        opts.on_tail('-q', '--quiet', 'Show no warnings') { log.level = Logger::ERROR }
-        opts.on_tail('--verbose', 'Show debugging information') { log.level = Logger::DEBUG }
-        opts.on_tail('-v', '--version', 'Show version') { puts "yard #{YARD::VERSION}"; exit }
-        opts.on_tail('-h', '--help', 'Show this help')  { puts opts; exit }
+        opts.separator ""
+        opts.separator "Other options:"
+        opts.on_tail('-q', '--quiet', 'Show no warnings.') { log.level = Logger::ERROR }
+        opts.on_tail('--verbose', 'Show debugging information.') { log.level = Logger::DEBUG }
+        opts.on_tail('-v', '--version', 'Show version.') { puts "yard #{YARD::VERSION}"; exit }
+        opts.on_tail('-h', '--help', 'Show this help.')  { puts opts; exit }
         
         begin
           opts.parse!(args)
@@ -93,7 +116,7 @@ module YARD
         end
         
         # Last minute modifications
-        self.files = args
+        self.files = args unless args.empty?
         self.reload = false if self.files.empty?
         visibilities.uniq!
         options[:serializer] = Serializers::FileSystemSerializer.new(serialopts)
