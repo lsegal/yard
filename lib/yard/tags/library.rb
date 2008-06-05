97a5bfa7adbab27e6d280d662d7637819e009dc3
Loren Segal <lsegal@soen.ca>
2008-06-04 23:18:01 -0400

    Refactor factory methods and add @default tag

--- lib/yard/tags/library.rb
+++ lib/yard/tags/library.rb
@@ -59,43 +59,49 @@ module YARD
         # @param [#to_s] tag the tag name to create
         # @param meth the {Tag} factory method to call when creating the tag
         def define_tag(label, tag, meth = "")
-          meth = meth.to_s
-          send_name = "parse_tag" + (meth.empty? ? "" : "_" + meth)
-          
-          class_eval(<<-eof, __FILE__, __LINE__)
-            def #{tag}_tag(text, raw_text) 
-              if @factory.respond_to? :#{send_name}
-                arity = @factory.method(:#{send_name}).arity
-                @factory.#{send_name} #{tag.inspect}, text, *(arity == 3 ? [raw_text] : [])
-              else
-                raise NoMethodError, "Factory \#{@factory.class_name} does not implement factory method :#{meth}."
-              end
-            end
-          eof
-          
+          define_method("#{tag}_tag") do |text, raw_text| 
+            send_to_factory(tag, meth, text, raw_text)
+          end
+
           @labels ||= SymbolHash.new(false)
           @labels.update(tag => label)
           tag
         end
       end
       
+      private
+      
+      def send_to_factory(tag_name, meth, text, raw_text)
+        meth = meth.to_s
+        send_name = "parse_tag" + (meth.empty? ? "" : "_" + meth)
+        if @factory.respond_to?(send_name)
+          arity = @factory.method(send_name).arity
+          @factory.send send_name, tag_name, text, *(arity == 3 ? [raw_text] : [])
+        else
+          raise NoMethodError, "Factory #{@factory.class_name} does not implement factory method :#{meth}."
+        end
+      end
+      
+      public
+      
       def initialize(factory = Library.default_factory)
         @factory = factory
       end
-
-      define_tag "Parameters",        :param,        :with_types_and_name
-      define_tag "Yield Parameters",  :yieldparam,   :with_types_and_name
-      define_tag "Yields",            :yield
-      define_tag "Returns",           :return,       :with_types
+      
+      define_tag "Parameters",        :param,       :with_types_and_name
+      define_tag "Yield Parameters",  :yieldparam,  :with_types_and_name
+      define_tag "Yields",            :yield,       :with_types
+      define_tag "Default Value",     :default,     :with_name
+      define_tag "Returns",           :return,      :with_types
       define_tag "Deprecated",        :deprecated
       define_tag "Author",            :author
-      define_tag "Raises",            :raise,        :with_name
+      define_tag "Raises",            :raise,       :with_name
       define_tag "See Also",          :see
       define_tag "Since",             :since
       define_tag "Version",           :version
       define_tag "API Visibility",    :api
       define_tag "Todo Item",         :todo      
-      define_tag "Example",           :example,      :with_raw_title_and_text
+      define_tag "Example",           :example,     :with_raw_title_and_text
     end
   end
 end
