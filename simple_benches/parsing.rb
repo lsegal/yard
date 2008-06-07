3dfbbf97d5478cefd28dca1879dd4bfbcb7ab462
Loren Segal <lsegal@soen.ca>
2008-06-07 01:34:43 -0400

    Log results from load_order changes

--- simple_benches/parsing.rb
+++ simple_benches/parsing.rb
@@ -5,3 +5,17 @@ Benchmark.bmbm do |x|
   x.report("parse in order") { YARD::Registry.clear; YARD.parse YARD::PATH_ORDER, Logger::ERROR } 
   x.report("parse") { YARD::Registry.clear; YARD.parse 'lib/**/*.rb', Logger::ERROR } 
 end
+
+=begin
+load_order branch (2008-06-07):
+
+Rehearsal --------------------------------------------------
+parse in order   6.510000   0.050000   6.560000 (  6.563223)
+parse            6.300000   0.040000   6.340000 (  6.362272)
+---------------------------------------- total: 12.900000sec
+
+                     user     system      total        real
+parse in order   6.310000   0.060000   6.370000 (  6.390945)
+parse            6.300000   0.050000   6.350000 (  6.366709)
+=end
+
