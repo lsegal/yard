0be5170f2366206fb09873ab0282157d308dbfcc
Loren Segal <lsegal@soen.ca>
2008-06-13 11:15:56 -0400

    Bench tiny and fast erubis as well

--- benchmarks/erb_vs_erubis.rb
+++ benchmarks/erb_vs_erubis.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'erubis'
+require 'erubis/tiny'
 require 'erb'
 require "benchmark"
 require File.join(File.dirname(__FILE__), '..', 'lib', 'yard')
@@ -10,20 +11,40 @@ def rungen
 end
 
 Benchmark.bmbm do |x|
-  x.report("erb")  do
+  x.report("erubis") do
     eval <<-eof
       class YARD::Generators::Base
-        def erb(str) ERB.new(str, nil, '<>') end
+        def erb(str) Erubis::Eruby.new(str) end
       end
     eof
     
     rungen
   end
-  
-  x.report("erubis") do
+
+  x.report("fast-erubis") do
     eval <<-eof
       class YARD::Generators::Base
-        def erb(str) Erubis::Eruby.new(str) end
+        def erb(str) Erubis::FastEruby.new(str) end
+      end
+    eof
+    
+    rungen
+  end
+
+  x.report("tiny-erubis") do
+    eval <<-eof
+      class YARD::Generators::Base
+        def erb(str) Erubis::TinyEruby.new(str) end
+      end
+    eof
+    
+    rungen
+  end
+
+  x.report("erb")  do
+    eval <<-eof
+      class YARD::Generators::Base
+        def erb(str) ERB.new(str, nil, '<>') end
       end
     eof
     
