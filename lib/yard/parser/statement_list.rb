c856637e483e6c0542a1d93fc55bced14609a5ae
Nathan Weizenbaum <nex342@gmail.com>
2009-03-31 19:05:10 -0700

    Parser::StatementList: Convert to instance vars.

--- lib/yard/parser/statement_list.rb
+++ lib/yard/parser/statement_list.rb
@@ -38,74 +38,75 @@ module YARD
       # WARNING WARNING WARNING             WARNING
       # THIS IS MEANT TO BE UGLY.
       def next_statement
-        statement, block, comments = TokenList.new, nil, nil
-        stmt_number, level = 0, 0
-        new_statement, open_block = true, false
-        last_tk, last_ns_tk, before_last_tk = nil, nil, nil
-        open_parens = 0
+        @statement, @block, @comments = TokenList.new, nil, nil
+        @stmt_number, @level = 0, 0
+        @new_statement, @open_block = true, false
+        @last_tk, @last_ns_tk, @before_last_tk = nil, nil, nil
+        @open_parens = 0
 
         while tk = @tokens.shift
           #p tk.class
           # !!!!!!!!!!!!!!!!!!!! REMOVED TkfLPAREN, TkfLBRACK
-          open_parens += 1 if [TkLPAREN, TkLBRACK].include? tk.class
-          open_parens -= 1 if [TkRPAREN, TkRBRACK].include?(tk.class) 
+          @open_parens += 1 if [TkLPAREN, TkLBRACK].include? tk.class
+          @open_parens -= 1 if [TkRPAREN, TkRBRACK].include? tk.class
       
-          #if open_parens < 0 || level < 0
-          #  STDERR.puts block.to_s + " TOKEN #{tk.inspect}"
+          #if @open_parens < 0 || @level < 0
+          #  STDERR.puts @block.to_s + " TOKEN #{tk.inspect}"
           #  exit
           #end
 
           # Get the initial comments
-          if statement.empty?
+
+          if @statement.empty?
             # Two new-lines in a row will destroy any comment blocks
-            if [TkCOMMENT].include?(tk.class)  && last_tk.class == TkNL && 
-              (before_last_tk && (before_last_tk.class == TkNL || before_last_tk.class == TkSPACE))
-              comments = nil
+            if [TkCOMMENT].include?(tk.class)  && @last_tk.class == TkNL && 
+              (@before_last_tk && (@before_last_tk.class == TkNL || @before_last_tk.class == TkSPACE))
+              @comments = nil
             elsif tk.class == TkCOMMENT
               # Remove the "#" and up to 1 space before the text
               # Since, of course, the convention is to have "# text"
               # and not "#text", which I deem ugly (you heard it here first)
-              comments ||= []
-              comments << tk.text.gsub(/^#+\s{0,1}/, '') 
-              comments.pop if comments.size == 1 && comments.first =~ /^\s*$/
+              @comments ||= []
+              @comments << tk.text.gsub(/^#+\s{0,1}/, '') 
+              @comments.pop if @comments.size == 1 && @comments.first =~ /^\s*$/
             end
           end
                 
           # Ignore any initial comments or whitespace
-          unless statement.empty? && stmt_number == 0 && [TkSPACE, TkNL, TkCOMMENT].include?(tk.class)
+          unless @statement.empty? && @stmt_number == 0 && [TkSPACE, TkNL, TkCOMMENT].include?(tk.class)
             # Decrease if end or '}' is seen
-            level -= 1 if [TkEND, TkRBRACE].include?(tk.class)
+            @level -= 1 if [TkEND, TkRBRACE].include?(tk.class)
         
             # Increase level if we have a 'do' or block opening
             if [TkLBRACE, TkDO, TkBEGIN].include?(tk.class)
-              #p "#{tk.line_no} #{level} #{tk} \t#{tk.text} #{tk.lex_state}"
-              stmt_number += 1
-              new_statement = true
-              level += 1    
+              #p "#{tk.line_no} #{@level} #{tk} \t#{tk.text} #{tk.lex_state}"
+              @stmt_number += 1
+              @new_statement = true
+              @level += 1    
             end
  
             # If the level is greater than 0, add the code to the block text
             # otherwise it's part of the statement text
-            if stmt_number > 0
-              #puts "Block of #{statement}"
-              #puts "#{stmt_number} #{tk.line_no} #{level} #{open_parens} #{tk.class.class_name} \t#{tk.text.inspect} #{tk.lex_state} #{open_block.inspect}" 
-              block ||= TokenList.new
-              block << tk
-            elsif stmt_number == 0 && tk.class != TkNL && tk.class != TkSEMICOLON && tk.class != TkCOMMENT
-              statement << tk 
+            if @stmt_number > 0
+              #puts "Block of #{@statement}"
+              #puts "#{@stmt_number} #{tk.line_no} #{@level} #{@open_parens} #{tk.class.class_name} \t#{tk.text.inspect} #{tk.lex_state} #{@open_block.inspect}" 
+              @block ||= TokenList.new
+              @block << tk
+            elsif @stmt_number == 0 && tk.class != TkNL && tk.class != TkSEMICOLON && tk.class != TkCOMMENT
+              @statement << tk 
             end
 
-            #puts "#{tk.line_no} #{level} #{open_parens} #{tk.class.class_name} \t#{tk.text.inspect} #{tk.lex_state} #{open_block.inspect}" 
+            #puts "#{tk.line_no} #{@level} #{@open_parens} #{tk.class.class_name} \t#{tk.text.inspect} #{tk.lex_state} #{@open_block.inspect}" 
 
             # Vouch to open a block when this statement would otherwise end
-            open_block = [level, tk.class] if (new_statement || 
-                                (last_tk && last_tk.lex_state == EXPR_BEG)) && 
-                                 OPEN_BLOCK_TOKENS.include?(tk.class)
+            @open_block = [@level, tk.class] if (@new_statement ||
+              (@last_tk && @last_tk.lex_state == EXPR_BEG)) && 
+              OPEN_BLOCK_TOKENS.include?(tk.class)
 
             # Check if this token creates a new statement or not
-            #puts "#{open_parens} open brackets for: #{statement.to_s}"
-            if open_parens == 0 && ((last_tk && [TkSEMICOLON, TkNL, TkEND_OF_SCRIPT].include?(tk.class)) ||
-              (open_block && open_block.last == TkDEF && tk.class == TkRPAREN))
+            #puts "#{@open_parens} open brackets for: #{@statement.to_s}"
+            if @open_parens == 0 && ((@last_tk && [TkSEMICOLON, TkNL, TkEND_OF_SCRIPT].include?(tk.class)) ||
+              (@open_block && @open_block.last == TkDEF && tk.class == TkRPAREN))
               
               # Make sure we don't have any running expressions
               # This includes things like
@@ -115,52 +116,52 @@ module YARD
               # 
               # if a ||
               #    b
-              if (last_tk && [EXPR_END, EXPR_ARG].include?(last_tk.lex_state)) || 
-                  (open_block && [TkNL, TkSEMICOLON].include?(tk.class) && last_ns_tk.class != open_block.last)
-                stmt_number += 1
-                new_statement = true
-                #p "NEW STATEMENT #{block.to_s}"
+              if (@last_tk && [EXPR_END, EXPR_ARG].include?(@last_tk.lex_state)) || 
+                  (@open_block && [TkNL, TkSEMICOLON].include?(tk.class) && @last_ns_tk.class != @open_block.last)
+                @stmt_number += 1
+                @new_statement = true
+                #p "NEW STATEMENT #{@block.to_s}"
 
                 # The statement started with a if/while/begin, so we must go to the next level now
-                if open_block && open_block.first == level
-                  if tk.class == TkNL && block.nil?
-                    block = TokenList.new
-                    block << tk
+                if @open_block && @open_block.first == @level
+                  if tk.class == TkNL && @block.nil?
+                    @block = TokenList.new
+                    @block << tk
                   end
 
-                  open_block = false
-                  level += 1
+                  @open_block = false
+                  @level += 1
                 end
               end
             elsif tk.class != TkSPACE
-              new_statement = false 
+              @new_statement = false 
             end
 
             # Else keyword is kind of weird
             if tk.is_a? TkELSE
-              new_statement = true
-              stmt_number += 1
-              open_block = false
+              @new_statement = true
+              @stmt_number += 1
+              @open_block = false
             end
 
             # We're done if we've ended a statement and we're at level 0
-            break if new_statement && level == 0
+            break if @new_statement && @level == 0
         
-            #raise "Unexpected end" if level < 0
+            #raise "Unexpected end" if @level < 0
           end
-      
-          #break if new_statement && level == 0
 
-          before_last_tk = last_tk
-          last_tk = tk # Save last token
-          last_ns_tk = tk unless [TkSPACE, TkNL, TkEND_OF_SCRIPT].include? tk.class
+          #break if @new_statement && @level == 0
+
+          @before_last_tk = @last_tk
+          @last_tk = tk # Save last token
+          @last_ns_tk = tk unless [TkSPACE, TkNL, TkEND_OF_SCRIPT].include? tk.class
         end
 
         # Return the code block with starting token and initial comments
         # If there is no code in the block, return nil
-        comments = comments.compact if comments
-        if block || !statement.empty?
-          Statement.new(statement, block, comments)
+        @comments = @comments.compact if @comments
+        if @block || !@statement.empty?
+          Statement.new(@statement, @block, @comments)
         else
           nil
         end
