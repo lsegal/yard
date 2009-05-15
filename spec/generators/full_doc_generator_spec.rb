03ff9e1fa0f73f61647d4a829365fac3f8fc5545
Loren Segal <lsegal@soen.ca>
2009-05-15 19:26:45 -0400

    Fix broken spec in YARD::Generators::FullDocGenerator

--- spec/generators/full_doc_generator_spec.rb
+++ spec/generators/full_doc_generator_spec.rb
@@ -1,19 +1,17 @@
 require File.join(File.dirname(__FILE__), *%w|.. spec_helper|)
 
-describe YARD::Generators::FullDocGenerator do
-  def publicize(obj, meth)
-    class <<obj; self; end.instance_eval { public meth }
-  end
+class YARD::Generators::FullDocGenerator
+  public :generate_files, :readme_file_exists? # public for test
+end
 
+describe YARD::Generators::FullDocGenerator do
   it "should know about absence of 'readme' file" do
     generator = Generators::FullDocGenerator.new(:readme => nil)
-    publicize generator, :readme_file_exists?
     generator.readme_file_exists?.should == false
   end
 
   it "should know about existence of 'readme' file" do
     generator = Generators::FullDocGenerator.new(:readme => __FILE__)
-    publicize generator, :readme_file_exists?
     generator.readme_file_exists?.should == true
   end
 
@@ -25,11 +23,7 @@ describe YARD::Generators::FullDocGenerator do
       :readme     => nil
     }
     generator = Generators::FullDocGenerator.new(generator_options)
-
-    generator.should_receive(:readme_file_exists?).once.and_return(false)
     serializer.should_not_receive(:serialize)
-
-    publicize generator, :generate_readme
-    lambda { generator.generate_readme }.should_not raise_error(Errno::ENOENT)
+    lambda { generator.generate_files }.should_not raise_error(Errno::ENOENT)
   end
 end
