1e47725f0e1d9812f527319ecb5c086415d6b0b2
Loren Segal <lsegal@soen.ca>
2009-05-20 02:35:41 -0400

    Fix references to RubyToken in Legacy parser specs

--- spec/parser/source_parser_spec.rb
+++ spec/parser/source_parser_spec.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require File.dirname(__FILE__) + '/../../lib/yard/parser/ruby/legacy/ruby_lex'
 
 describe YARD::Parser::SourceParser do
   before do 
