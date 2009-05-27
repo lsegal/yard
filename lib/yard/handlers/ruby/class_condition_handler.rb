7d98f468e8c0cf96d6891097a513619d987acbc5
Loren Segal <lsegal@soen.ca>
2009-05-26 20:29:13 -0400

    Add support for if true/false/0 conditionals in class scope, also if defined? CONST, which Rails uses a lot

--- lib/yard/handlers/ruby/class_condition_handler.rb
+++ lib/yard/handlers/ruby/class_condition_handler.rb
@@ -3,12 +3,68 @@ class YARD::Handlers::Ruby::ClassConditionHandler < YARD::Handlers::Ruby::Base
   handles meta_type(:condition)
   
   def process
-    # TODO we have the opportunity to be smart about literal conditionals
-    # here. Currently we parse the inner block(s) no matter what, but we
-    # could theoretically avoid parsing things like +if 0+ or even constants
-    # that have been previously defined (it would be a fair assumption to
-    # treat constants as having "true" static constant value).
+    condition = parse_condition
+    if condition == nil
+      # Parse both blocks if we're unsure of the condition
+      parse_then_block
+      parse_else_block
+    elsif condition
+      parse_then_block
+    else
+      parse_else_block
+    end
+  end
+  
+  protected
+  
+  # Parses the condition part of the if/unless statement
+  # 
+  # @return [true, false, nil] true if the condition can be definitely
+  #   parsed to true, false if not, and nil if the condition cannot be
+  #   parsed with certainty (it's dynamic)
+  def parse_condition
+    condition = nil
+    
+    # Right now we can handle very simple unary conditions like:
+    #   if true
+    #   if false
+    #   if 0
+    #   if 100 (not 0)
+    #   if defined? SOME_CONSTANT
+    # 
+    # The last case will do a lookup in the registry and then one
+    # in the Ruby world (using eval).
+    case statement.condition.type
+    when :int
+      condition = statement.condition[0] != "0"
+    when :defined
+      # defined? keyword used, let's see if we can look up the name
+      # in the registry, then we'll try using Ruby's powers. eval() is not
+      # *too* dangerous here since code is not actually executed.
+      name = statement.condition[0].source
+      obj = YARD::Registry.resolve(namespace, name, true)
+      condition = true if obj || Object.instance_eval("defined? #{name}")
+    when :var_ref
+      var = statement.condition[0]
+      if var == s(:kw, "true")
+        condition = true
+      elsif var == s(:kw, "false")
+        condition = false
+      end
+    end
+    
+    # Invert an unless condition
+    if statement.type == :unless || statement.type == :unless_mod
+      condition = !condition if condition != nil
+    end
+    condition
+  end
+  
+  def parse_then_block
     parse_block(statement.then_block)
+  end
+  
+  def parse_else_block
     parse_block(statement.else_block) if statement.else_block
   end
 end
--- spec/handlers/class_condition_handler_spec.rb
+++ spec/handlers/class_condition_handler_spec.rb
@@ -8,17 +8,42 @@ describe "YARD::Handlers::Ruby::#{RUBY18 ? "Legacy::" : ""}ClassConditionHandler
   
   def verify_method(*names)
     names.each {|name| Registry.at("A##{name}").should_not be_nil }
+    names.each {|name| Registry.at("A##{name}not").should be_nil }
   end
   
-  it "should parse all if/elsif blocks regardless of condition" do
+  it "should parse all if/elsif blocks for complex conditions" do
     verify_method :a, :b, :c, :d
   end
   
-  it "should parse all unless blocks regardless of condition" do
-    verify_method :e, :f, :g
+  it "should parse all unless blocks for complex conditions" do
+    verify_method :g
   end
   
   it "should not parse conditionals inside methods" do
-    Registry.at('A#i').should be_nil
+    verify_method :h
+  end
+  
+  it "should only parse then block if condition is literal value `true`" do
+    verify_method :p
+  end
+  
+  it "should only parse else block if condition is literal value `false`" do
+    verify_method :q
+  end
+
+  it "should only parse then block if condition is literal integer != 0" do
+    verify_method :o
+  end
+
+  it "should only parse else block if condition is literal integer == 0" do
+    verify_method :n
+  end
+  
+  it "should invert block to parse for literal condition if it's an unless block" do
+    verify_method :e
+  end
+  
+  it "should handle conditions such as 'defined? VALUE'" do
+    verify_method :j, :k
   end
 end if RUBY19
