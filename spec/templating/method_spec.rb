d2d2a7e523170ef43e577dc273c1f8e4d3ad1378
Loren Segal <lsegal@soen.ca>
2009-08-14 02:34:33 -0400

    Refactor specs into separate example files

--- spec/templating/method_spec.rb
+++ spec/templating/method_spec.rb
@@ -1,19 +1,8 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-def html_equals(result, expected)
-  [expected, result].each do |value|
-    value.gsub!(/(>)\s+|\s+(<)/, '\1\2')
-    value.strip!
-  end
-  result.should == expected
-end
-
 describe Template.template(:default, :method) do
   before { Registry.clear }
   
-  def source(line)
-  end
-
   describe 'regular (deprecated) method' do
     it "should render correctly" do
       YARD.parse_string <<-'eof'
@@ -26,65 +15,7 @@ describe Template.template(:default, :method) do
         alias x m
       eof
 
-      html_equals(Registry.at('#m').format(:format => :html), <<-'eof')
-        <div id="m-instance_method" class="section method"> 
-          <div class="details_title"> 
-            <div class='section signature'> 
-            <tt class='def'> 
-              <span class='visibility'>private</span> 
-              <span class='return_types'><tt>String</tt></span> 
-              <span class='name'>m</span><span class='args'>(x)</span> 
-              <span class='block'></span> 
-            </tt> 
-          </div> 
-          <p class="aliases"> 
-            <span class="aka">Also known as:</span> 
-            <tt id="x-instance_method">x</tt> 
-          </p> 
-          </div><div class="deprecated"> 
-            <p> 
-              <strong>Deprecated.</strong> <em>for great justice</em> 
-            </p> 
-          </div> 
-          <div class="docstring">Comments</div>
-          <div class="tags"> 
-            <div class="param"> 
-              <h3>Parameters:</h3> 
-              <dl> 
-                <dt> 
-                  <span class='type'>[<tt>String</tt>]</span> 
-                  <span class='name'>x</span> 
-                </dt>
-                <dd><span class='desc'>the x argument</span></dd> 
-              </dl> 
-            </div>
-            <div class="return"> 
-              <h3>Returns:</h3> 
-              <dl> 
-                <dt> 
-                  <span class='type'>[<tt>String</tt>]</span> 
-                  <span class='name'></span> 
-                </dt> 
-                <dd><span class='desc'>the result</span></dd> 
-              </dl> 
-            </div> 
-          </div>
-          <div class="section source"> 
-            <span>[<a class="source_link" href="#">View source</a>]</span> 
-            <div class="source_code" style="display: none"> 
-              <table> 
-                <tr> 
-                  <td><pre class="lines">6</pre></td> 
-                  <td><pre class="code"><span class="info file"># File '(stdin)', line 6</span> 
-                  <span class='kw'>def</span> <span class='id m'>m</span>
-                  <span class='lparen'>(</span><span class='id x'>x</span>
-                  <span class='rparen'>)</span> <span class='kw'>end</span></pre></td> 
-                </tr> 
-              </table> 
-            </div> 
-          </div> 
-        </div>
-      eof
+      html_equals(Registry.at('#m').format(:format => :html), :method001)
     end
   end
   
@@ -99,52 +30,7 @@ describe Template.template(:default, :method) do
         def m(x) end
       eof
 
-      html_equals(Registry.at('#m').format(:format => :html), <<-'eof')
-        <div id="m-instance_method" class="section method"> 
-          <div class="details_title"> 
-            <div class='section signature'> 
-              <tt class='def'> 
-                <span class='visibility'>private</span> 
-                <span class='return_types'></span> 
-                <span class='name'>m</span><span class='args'>(x, y)</span> 
-                <span class='block'></span> 
-              </tt> 
-            </div> 
-          </div>
-          <div class="docstring">Comments</div>
-          <div class="tags">
-            <div class="param">
-              <h3>Parameters:</h3> 
-              <dl> 
-                <dt> 
-                  <span class='type'>[<tt>String</tt>]</span> 
-                  <span class='name'>x</span> 
-                </dt>
-                <dd><span class='desc'>parameter x</span></dd> 
-                <dt> 
-                  <span class='type'>[<tt>Boolean</tt>]</span> 
-                  <span class='name'>y</span> 
-                </dt>
-                <dd><span class='desc'>parameter y</span></dd> 
-              </dl> 
-            </div>
-          </div>
-          <div class="section source"> 
-            <span>[<a class="source_link" href="#">View source</a>]</span> 
-            <div class="source_code" style="display: none"> 
-              <table> 
-                <tr> 
-                  <td><pre class="lines">6</pre></td> 
-                  <td><pre class="code"><span class="info file"># File '(stdin)', line 6</span> 
-                  <span class='kw'>def</span> <span class='id m'>m</span>
-                  <span class='lparen'>(</span><span class='id x'>x</span>
-                  <span class='rparen'>)</span> <span class='kw'>end</span></pre></td> 
-                </tr> 
-              </table> 
-            </div> 
-          </div> 
-        </div>
-      eof
+      html_equals(Registry.at('#m').format(:format => :html), :method002)
     end
   end
   
@@ -164,100 +50,7 @@ describe Template.template(:default, :method) do
         def m(*args) end
       eof
 
-      html_equals(Registry.at('#m').format(:format => :html), <<-'eof')
-        <div id="m-instance_method" class="section method"> 
-          <div class="details_title"> 
-            <div class='section signature'> 
-              <tt class='def'> 
-                <span class='visibility'>private</span> 
-                <span class='return_types'></span> 
-                <span class='name'>m</span><span class='args'>(*args)</span> 
-                <span class='block'></span> 
-              </tt> 
-            </div> 
-          </div>
-          <div class="docstring">Method comments</div>
-          <div class="tags"></div>
-          <div id="m-instance_method_overload2" class="section method overload"> 
-            <div class="details_title"> 
-              <div class='section signature'> 
-                <tt class='def'> 
-                  <span class='return_types'></span> 
-                  <span class='name'>m</span><span class='args'>(x, y)</span> 
-                  <span class='block'></span> 
-                </tt> 
-              </div> 
-            </div>
-            <div class="docstring">Overload docstring</div>
-            <div class="tags">
-              <div class="param">
-                <h3>Parameters:</h3> 
-                <dl> 
-                  <dt> 
-                    <span class='type'>[<tt>String</tt>]</span> 
-                    <span class='name'>x</span> 
-                  </dt>
-                  <dd><span class='desc'>parameter x</span></dd> 
-                  <dt> 
-                    <span class='type'>[<tt>Boolean</tt>]</span> 
-                    <span class='name'>y</span> 
-                  </dt>
-                  <dd><span class='desc'>parameter y</span></dd> 
-                </dl> 
-              </div>
-            </div>
-          </div>
-          <div id="m-instance_method_overload3" class="section method overload"> 
-            <div class="details_title"> 
-              <div class='section signature'> 
-                <tt class='def'> 
-                  <span class='return_types'></span> 
-                  <span class='name'>m</span><span class='args'>(x, y, z)</span> 
-                  <span class='block'></span> 
-                </tt> 
-              </div> 
-            </div>
-            <div class="docstring"></div>
-            <div class="tags">
-              <div class="param">
-                <h3>Parameters:</h3> 
-                <dl> 
-                  <dt> 
-                    <span class='type'>[<tt>String</tt>]</span> 
-                    <span class='name'>x</span> 
-                  </dt>
-                  <dd><span class='desc'>parameter x</span></dd> 
-                  <dt> 
-                    <span class='type'>[<tt>Boolean</tt>]</span> 
-                    <span class='name'>y</span> 
-                  </dt>
-                  <dd><span class='desc'>parameter y</span></dd> 
-                  <dt> 
-                    <span class='type'>[<tt>Boolean</tt>]</span> 
-                    <span class='name'>z</span> 
-                  </dt>
-                  <dd><span class='desc'>parameter z</span></dd> 
-                </dl> 
-              </div>
-            </div>
-          </div>
-          <div class="section source"> 
-            <span>[<a class="source_link" href="#">View source</a>]</span> 
-            <div class="source_code" style="display: none"> 
-              <table> 
-                <tr> 
-                  <td><pre class="lines">11</pre></td> 
-                  <td><pre class="code"><span class="info file"># File '(stdin)', line 11</span> 
-                  <span class='kw'>def</span> <span class='id m'>m</span>
-                  <span class='lparen'>(</span><span class='op'>*</span>
-                  <span class='id args'>args</span>
-                  <span class='rparen'>)</span> <span class='kw'>end</span></pre></td> 
-                </tr> 
-              </table> 
-            </div> 
-          </div> 
-        </div>
-      eof
+      html_equals(Registry.at('#m').format(:format => :html), :method003)
     end
   end
 end
--- spec/templating/spec_helper.rb
+++ spec/templating/spec_helper.rb
@@ -1,10 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-def html_equals(result, expected)
+def html_equals(result, expected_example)
+  html_equals_string(result, example(expected_example))
+end
+
+def html_equals_string(result, expected)
   [expected, result].each do |value|
-    value.gsub!(/\s+/, ' ')
     value.gsub!(/(>)\s+|\s+(<)/, '\1\2')
     value.strip!
   end
   result.should == expected
+end
+
+def example(filename)
+  File.read(File.join(File.dirname(__FILE__), 'examples', "#{filename}.html"))
 end
