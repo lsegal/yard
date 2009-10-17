a87d1660b112c334632e1381db0d9a69637de667
Loren Segal <lsegal@soen.ca>
2009-10-17 02:01:39 -0400

    Add tests for YARD::Templates::Template.full_paths

---
+++ spec/templates/template_spec.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe YARD::Templates::Template do
+  def template(path)
+    YARD::Templates::Engine.template!(path, '/full/path/' + path.to_s)
+  end
+  
+  describe '.full_paths' do
+    it "should list full_path" do
+      mod = template(:a)
+      mod.full_paths.should == [Pathname.new('/full/path/a')]
+    end
+    
+    it "should list paths of included modules" do
+      mod = template(:a)
+      mod.send(:include, template(:b))
+      mod.full_paths.should == [Pathname.new('/full/path/a'), Pathname.new('/full/path/b')]
+    end
+    
+    it "should list paths from modules of included modules" do
+      mod = template(:c)
+      mod.send(:include, template(:d))
+      mod.send(:include, template(:a))
+      mod.full_paths.should == ['c', 'a', 'b', 'd'].map {|o| Pathname.new('/full/path/' + o) }
+    end
+    
+    it "should only list full paths of modules that respond to full_paths" do
+      mod = template(:d)
+      mod.send(:include, Enumerable)
+      mod.full_paths.should == [Pathname.new('/full/path/d')]
+    end
+  end
+end
