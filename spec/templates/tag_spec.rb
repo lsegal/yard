a362b8402e87eb55fbcf7005158e6cd7abac0661
Loren Segal <lsegal@soen.ca>
2009-10-18 19:42:02 -0400

    Add text templates for tags

---
+++ spec/templates/tag_spec.rb
@@ -0,0 +1,33 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe Engine.template(:default, :tags) do
+  before { Registry.clear }
+  
+  describe 'all known tags' do
+    before do
+      YARD.parse_string <<-'eof'
+        # Comments
+        # @param [Hash] opts the options
+        # @option opts :key ('') hello
+        # @option opts :key2 (X) hello
+        # @return [String] the result
+        # @raise [Exception] Exception class
+        # @deprecated for great justice
+        # @see A
+        # @see http://url.com
+        # @see http://url.com Example
+        # @author Name
+        # @since 1.0
+        # @version 1.0
+        # @yield a block
+        # @yieldparam [String] a a value
+        # @yieldreturn [Hash] a hash
+        def m(opts = {}) end
+      eof
+    end
+
+    it "should render text format correctly" do
+      text_equals(Registry.at('#m').format, :tag001)
+    end
+  end
+end
--- templates/default/method/text/setup.rb
+++ templates/default/method/text/setup.rb
@@ -7,22 +7,3 @@ def format_object_title(object)
   title += " (#{object.namespace})" if object.namespace != Registry.root
   title
 end
-
-def signature(meth)
-  type = (meth.tag(:return) && meth.tag(:return).types ? meth.tag(:return).types.first : nil) || "Object"
-  scope = meth.scope == :class ? "#{meth.namespace.name}." : "#{meth.namespace.name.to_s.downcase}."
-  name = meth.name
-  blk = format_block(meth)
-  args = format_args(meth)
-  extras = []
-  extras_text = ''
-  if rw = meth.namespace.attributes[meth.scope][meth.name]
-    attname = [rw[:read] ? 'read' : nil, rw[:write] ? 'write' : nil].compact
-    attname = attname.size == 1 ? attname.join('') + 'only' : nil
-    extras << attname if attname
-  end
-  extras << meth.visibility if meth.visibility != :public
-  extras_text = '(' + extras.join(", ") + ')' unless extras.empty?
-  title = "%s%s%s %s -> %s %s" % [scope, name, args, blk, type, extras_text]
-  title.gsub(/\s+/, ' ')
-end
