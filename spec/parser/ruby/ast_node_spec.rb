aabdae7c3d2dc60e9ae16b705cbb4b58efa7a550
Loren Segal <lsegal@soen.ca>
2009-10-17 22:34:15 -0400

    Test pretty print

--- spec/parser/ruby/ast_node_spec.rb
+++ spec/parser/ruby/ast_node_spec.rb
@@ -1,4 +1,6 @@
 require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
+require 'pp'
+require 'stringio'
 
 include YARD::Parser::Ruby
 
@@ -13,6 +15,15 @@ if RUBY19
       it "should return the original ast if no inner node is found" do
         ast = s(:paren, s(:list, s(:list, s(s(:ident, "hi"), s(:ident, "bye")))))
         ast.jump(:params).object_id.should == ast.object_id
+      end
+    end
+    
+    describe '#pretty_print' do
+      it "should show a list of nodes" do
+        out = StringIO.new
+        PP.pp(s(:paren, s(:list, s(:ident, "bye"), line: 1)), out)
+        out.rewind
+        out.read.should == "s(:paren,\n   s(s(:ident, \"bye\", line: 0...0, source: 0...0), line: 1, source: 0..0))\n"
       end
     end
   end
