b0fd99b36ca75ce4712f56d87b69ada820571187
Loren Segal <lsegal@soen.ca>
2009-10-20 21:56:34 -0400

    Documentation for globals

--- lib/yard/globals.rb
+++ lib/yard/globals.rb
@@ -1,7 +1,6 @@
 # Global methods
 
-# Shortcut for creating a YARD::CodeObjects::Proxy 
-# via a path
+# Shortcut for creating a YARD::CodeObjects::Proxy via a path
 # 
 # @see YARD::CodeObjects::Proxy
 # @see YARD::Registry#resolve
@@ -10,6 +9,10 @@ def P(namespace, name = nil)
   YARD::Registry.resolve(namespace, name, false, true)
 end
 
+# The global {YARD::Logger} instance
+# 
+# @return [YARD::Logger] the global {YARD::Logger} instance
+# @see YARD::Logger
 def log
   YARD::Logger.instance
 end
