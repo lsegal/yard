41703f6fceaba65445991bd78f6bbc0bd11d9dd7
Loren Segal <lsegal@soen.ca>
2009-10-21 17:56:09 -0400

    Documentation for RootObject

--- lib/yard/code_objects/root_object.rb
+++ lib/yard/code_objects/root_object.rb
@@ -1,5 +1,7 @@
 module YARD
   module CodeObjects
+    # Represents the root namespace object (the invisible Ruby module that
+    # holds all top level modules, class and other objects).
     class RootObject < ModuleObject
       def path; "" end
       def inspect; "#<yardoc root>" end
