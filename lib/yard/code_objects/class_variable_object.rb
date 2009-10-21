d76d933cf8abe40ee7a1884c2b566124f73d5bdc
Loren Segal <lsegal@soen.ca>
2009-10-21 17:37:52 -0400

    Documentation for ClassVariableObject

--- lib/yard/code_objects/class_variable_object.rb
+++ lib/yard/code_objects/class_variable_object.rb
@@ -1,4 +1,6 @@
 module YARD::CodeObjects
+  # Represents a class variable inside a namespace. The path is expressed
+  # in the form "A::B::@@classvariable"
   class ClassVariableObject < Base
   end
 end
