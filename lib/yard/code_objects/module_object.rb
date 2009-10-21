457b7464fa2111d37286aec9d2b11afe1129d626
Loren Segal <lsegal@soen.ca>
2009-10-21 17:55:11 -0400

    Documentation for ModuleObject

--- lib/yard/code_objects/module_object.rb
+++ lib/yard/code_objects/module_object.rb
@@ -1,5 +1,11 @@
 module YARD::CodeObjects
+  # Represents a Ruby module.
   class ModuleObject < NamespaceObject
+    # Returns the inheritance tree of mixins.
+    # 
+    # @param [Boolean] include_mods if true, will include mixed in
+    #   modules (which is likely what is wanted).
+    # @return [Array<NamespaceObject>] a list of namespace objects
     def inheritance_tree(include_mods = false)
       return [self] unless include_mods
       [self] + mixins(:instance).map do |m|
