25864c2896b179b1278b1b9c3a0f2f5240e74a62
Loren Segal <lsegal@soen.ca>
2009-10-22 15:37:48 -0400

    Documentation for YardocTask

--- lib/yard/rake/yardoc_task.rb
+++ lib/yard/rake/yardoc_task.rb
@@ -4,12 +4,35 @@ require 'rake/tasklib'
 module YARD
   module Rake
     
+    # The rake task to run {CLI::Yardoc} and generate documentation.
     class YardocTask < ::Rake::TaskLib
+      # The name of the task
+      # @return [String] the task name
       attr_accessor :name
+      
+      # Options to pass to {CLI::Yardoc}
+      # @return [Hash] the options passed to the commandline utility
       attr_accessor :options
+      
+      # The Ruby source files (and any extra documentation files separated by '-')
+      # to process
+      # @return [Array<String>] a list of files
       attr_accessor :files
-      attr_accessor :before, :after
+      
+      # Runs a +Proc+ before the task
+      # @return [Proc] a proc to call before running the task
+      attr_accessor :before
+      
+      # Runs a +Proc+ after the task
+      # @return [Proc] a proc to call after running the task
+      attr_accessor :after
 
+      # Creates a new task with name +name+.
+      # 
+      # @param [String, Symbol] name the name of the rake task
+      # @yield a block to allow any options to be modified on the task
+      # @yieldparam [YardocTask] _self the task object to allow any parameters
+      #   to be changed.
       def initialize(name = :yard)
         @name = name
         @options = []
@@ -22,6 +45,10 @@ module YARD
         define
       end
       
+      protected
+      
+      # Defines the rake task
+      # @return [nil] 
       def define
         desc "Generate YARD Documentation"
         task(name) do
