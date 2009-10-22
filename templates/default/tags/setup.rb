bd96453edb42a21c4a4a3be993ba2ad342f76b33
Loren Segal <lsegal@soen.ca>
2009-10-22 02:32:59 -0400

    Reorder tags

--- templates/default/tags/setup.rb
+++ templates/default/tags/setup.rb
@@ -1,6 +1,6 @@
 def init
-  sections :index, [:example, :overload, [T('docstring')], :yields, :param, :option, 
-    :return, :yieldparam, :yieldreturn, :raises, :see, :author, :since, :version]
+  sections :index, [:example, :overload, [T('docstring')], :param, :option, :yields, 
+    :yieldparam, :yieldreturn, :return, :raises, :see, :author, :since, :version]
 end
 
 def param
