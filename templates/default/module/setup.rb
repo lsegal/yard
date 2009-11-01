8a057ee05f025fd048c04aeabf43e58a6ff62aa8
Loren Segal <lsegal@soen.ca>
2009-11-01 15:46:32 -0500

    Add abstract/deprecated info bubbles

--- templates/default/module/setup.rb
+++ templates/default/module/setup.rb
@@ -2,8 +2,9 @@ include Helpers::ModuleHelper
 
 def init
   sections :header, :box_info, :pre_docstring, T('docstring'), :children, 
-    :constant_summary, :inherited_constants, :attribute_summary, 
-    :method_summary, :inherited_methods,
+    :constant_summary, :inherited_constants, 
+    :attribute_summary, [:item_summary], 
+    :method_summary, [:item_summary], :inherited_methods,
     :methodmissing, [T('method_details')],
     :attribute_details, [T('method_details')], 
     :method_details_list, [T('method_details')]
