7e4c01fe692733bc4c58a96b44ecfee6d614735b
Loren Segal <lsegal@soen.ca>
2009-11-03 20:20:12 -0500

    Don't use perftools

--- benchmarks/template_profile.rb
+++ benchmarks/template_profile.rb
@@ -1,18 +1,17 @@
 require 'rubygems'
-#require 'ruby-prof'
+require 'ruby-prof'
 #require 'benchmark'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'yard')
 
 YARD::Registry.load_yardoc(File.join(File.dirname(__FILE__), '..', '.yardoc'))
 obj = YARD::Registry.at("YARD::CodeObjects::Base")
 
-require 'perftools'
-PerfTools::CpuProfiler.start("template_profile") do
+#PerfTools::CpuProfiler.start("template_profile") do
+#end
+
+result = RubyProf.profile do
   obj.format(:format => :html, :no_highlight => true)
 end
 
-# result = RubyProf.profile do
-# end
-# 
-# printer = RubyProf::CallTreePrinter.new(result)
-# printer.print(STDOUT)
+printer = RubyProf::CallTreePrinter.new(result)
+printer.print(STDOUT)
