9b499b9512a4b9c32e00c35f57a609346b43ec85
Loren Segal <lsegal@soen.ca>
2009-11-12 16:59:24 -0500

    Make String#underscore behave like activesupport's (Rails compatibility)
    Closes gh-35

--- lib/yard/core_ext/string.rb
+++ lib/yard/core_ext/string.rb
@@ -1,9 +1,20 @@
 class String
+  # Separates capital letters following lower case letters by an underscore
+  # and returns the entire string in lower case
+  # 
+  # @example
+  #   "FooBar".underscore # => "foo_bar"
+  # @return [String] the underscored lower case string
   def underscore
-    gsub(/([a-z])([A-Z])/, '\1_\2').downcase 
+    gsub(/([a-z])([A-Z])/, '\1_\2').downcase.gsub('::', '/')
   end
   
+  # Camel cases any underscored text.
+  # 
+  # @example
+  #   "foo_bar_baz".camelcase # => "FooBarBaz"
+  # @return [String] the camel cased text
   def camelcase
-    gsub(/([a-z])_([a-z])/i) { $1 + $2.upcase }.sub(/^(.)/) { $1.upcase } 
+    gsub(/([a-z])_([a-z])/i) { $1 + $2.upcase }.sub(/^(.)/) { $1.upcase }.gsub('/', '::') 
   end
 end
