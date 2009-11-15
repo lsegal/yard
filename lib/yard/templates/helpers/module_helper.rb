bcfaa0a8e47d1f6362ebc36523dbfc2a9a8dae26
Loren Segal <lsegal@soen.ca>
2009-11-15 05:04:27 -0500

    Fix prune_method_listing to work more gracefully with methods in proxy namespaces

--- lib/yard/templates/helpers/module_helper.rb
+++ lib/yard/templates/helpers/module_helper.rb
@@ -5,8 +5,8 @@ module YARD
         def prune_method_listing(list, hide_attributes = true)
           list = run_verifier(list)
           list = list.reject {|o| !options[:visibilities].include? o.visibility } if options[:visibilities]
-          list = list.reject(&:is_alias?)
-          list = list.reject(&:is_attribute?) if hide_attributes
+          list = list.reject {|o| o.is_alias? unless CodeObjects::Proxy === o.namespace }
+          list = list.reject {|o| o.is_attribute? unless CodeObjects::Proxy === o.namespace } if hide_attributes
           list
         end
       end
