a1bc172a24ea88356e3bc1094d5299c286300c12
Loren Segal <lsegal@soen.ca>
2009-12-08 17:49:55 -0500

    Refactor string specifications

--- spec/core_ext/string_spec.rb
+++ spec/core_ext/string_spec.rb
@@ -3,22 +3,24 @@ require File.dirname(__FILE__) + '/../spec_helper'
 #described_in_docs String, '#camelcase'
 #described_in_docs String, '#underscore'
 
-describe String, '#underscore' do
-  it 'should turn HelloWorld into hello_world' do
-    "HelloWorld".underscore.should == "hello_world"
-  end
+describe String do
+  describe '#underscore' do
+    it 'should turn HelloWorld into hello_world' do
+      "HelloWorld".underscore.should == "hello_world"
+    end
   
-  it "should turn Hello::World into hello/world" do
-    "Hello::World".underscore.should == "hello/world"
-  end
-end
-
-describe String, '#camelcase' do
-  it 'should turn hello_world into HelloWorld' do
-    "hello_world".camelcase.should == "HelloWorld"
+    it "should turn Hello::World into hello/world" do
+      "Hello::World".underscore.should == "hello/world"
+    end
   end
 
-  it "should turn hello/world into Hello::World" do
-    "Hello::World".underscore.should == "hello/world"
+  describe '#camelcase' do
+    it 'should turn hello_world into HelloWorld' do
+      "hello_world".camelcase.should == "HelloWorld"
+    end
+
+    it "should turn hello/world into Hello::World" do
+      "Hello::World".underscore.should == "hello/world"
+    end
   end
 end
