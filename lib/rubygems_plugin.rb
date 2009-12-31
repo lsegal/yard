54670515e6660b87d64983be1f1a29a0f1a456a5
Loren Segal <lsegal@soen.ca>
2009-12-31 15:29:47 -0500

    Fix yard gem plugin in 1.8.x with multiple versions of yard installed
    Closes gh-62

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -22,58 +22,58 @@ class Gem::Specification
   alias has_yardoc? has_yardoc
 end
 
-class Gem::DocManager
-  def self.load_yardoc
-    require File.dirname(__FILE__) + '/yard'
-  end
+unless defined? Gem::DocManager.load_yardoc
+  class Gem::DocManager
+    def self.load_yardoc
+      require File.dirname(__FILE__) + '/yard'
+    end
   
-  def run_yardoc(*args)
-    args << '--quiet'
-    args << @spec.require_paths
-    if @spec.extra_rdoc_files.size > 0
-      args << '-'
-      args += @spec.extra_rdoc_files
-    end
-    args = args.flatten.map {|arg| arg.to_s }
+    def run_yardoc(*args)
+      args << '--quiet'
+      args << @spec.require_paths
+      if @spec.extra_rdoc_files.size > 0
+        args << '-'
+        args += @spec.extra_rdoc_files
+      end
+      args = args.flatten.map {|arg| arg.to_s }
 
-    old_pwd = Dir.pwd
-    Dir.chdir(@spec.full_gem_path)
-    YARD::CLI::Yardoc.run(*args)
-  rescue Errno::EACCES => e
-    dirname = File.dirname e.message.split("-")[1].strip
-    raise Gem::FilePermissionError.new(dirname)
-  rescue => ex
-    alert_error "While generating documentation for #{@spec.full_name}"
-    ui.errs.puts "... MESSAGE:   #{ex}"
-    ui.errs.puts "... YARDDOC args: #{args.join(' ')}"
-    ui.errs.puts "\t#{ex.backtrace.join("\n\t")}" if Gem.configuration.backtrace
-    ui.errs.puts "(continuing with the rest of the installation)"
-  ensure
-    Dir.chdir(old_pwd)
-  end
-
-  undef setup_rdoc
-  def setup_rdoc
-    if File.exist?(@doc_dir) && !File.writable?(@doc_dir) then
-      raise Gem::FilePermissionError.new(@doc_dir)
+      old_pwd = Dir.pwd
+      Dir.chdir(@spec.full_gem_path)
+      YARD::CLI::Yardoc.run(*args)
+    rescue Errno::EACCES => e
+      dirname = File.dirname e.message.split("-")[1].strip
+      raise Gem::FilePermissionError.new(dirname)
+    rescue => ex
+      alert_error "While generating documentation for #{@spec.full_name}"
+      ui.errs.puts "... MESSAGE:   #{ex}"
+      ui.errs.puts "... YARDDOC args: #{args.join(' ')}"
+      ui.errs.puts "\t#{ex.backtrace.join("\n\t")}" if Gem.configuration.backtrace
+      ui.errs.puts "(continuing with the rest of the installation)"
+    ensure
+      Dir.chdir(old_pwd)
     end
 
-    FileUtils.mkdir_p @doc_dir unless File.exist?(@doc_dir)
+    undef setup_rdoc
+    def setup_rdoc
+      if File.exist?(@doc_dir) && !File.writable?(@doc_dir) then
+        raise Gem::FilePermissionError.new(@doc_dir)
+      end
 
-    self.class.load_rdoc if @spec.has_rdoc?
-    self.class.load_yardoc if @spec.has_yardoc?
-  end
+      FileUtils.mkdir_p @doc_dir unless File.exist?(@doc_dir)
 
-  def install_yardoc
-    rdoc_dir = File.join(@doc_dir, 'rdoc')
+      self.class.load_rdoc if @spec.has_rdoc?
+      self.class.load_yardoc if @spec.has_yardoc?
+    end
 
-    FileUtils.rm_rf rdoc_dir
+    def install_yardoc
+      rdoc_dir = File.join(@doc_dir, 'rdoc')
 
-    say "Installing YARD documentation for #{@spec.full_name}..."
-    run_yardoc '-o', rdoc_dir
-  end
+      FileUtils.rm_rf rdoc_dir
 
-  unless instance_methods.include?(:install_ri_yard)
+      say "Installing YARD documentation for #{@spec.full_name}..."
+      run_yardoc '-o', rdoc_dir
+    end
+
     def install_ri_yard
       install_ri_yard_orig if @spec.has_rdoc?
       return if @spec.has_yardoc?
