c427c0feed4b80ad10edf2140e9d239d8aea7c5b
Loren Segal <lsegal@soen.ca>
2010-06-20 15:32:59 -0400

    Add ability to build gem for single gem in  command

--- lib/yard/cli/gems.rb
+++ lib/yard/cli/gems.rb
@@ -3,6 +3,7 @@ module YARD
     class Gems < Command
       def initialize
         @rebuild = false
+        @gems = []
       end
       
       def description; "Builds YARD index for gems" end
@@ -14,17 +15,17 @@ module YARD
       # @return [void] 
       def run(*args)
         optparse(*args)
+        @gems += Gem.source_index.find_name('') if @gems.empty?
         build_gems
       end
       
       private
       
       # Builds .yardoc files for all non-existing gems
-      # @param [Boolean] rebuild Forces rebuild of all gems
+      # @param [Array] gems
       def build_gems
         require 'rubygems'
-        Gem.source_index.find_name('').each do |spec|
-          reload = true
+        @gems.each do |spec|
           dir = Registry.yardoc_file_for_gem(spec.name)
           if dir && File.directory?(dir) && !@rebuild
             log.debug "#{spec.name} index already exists at '#{dir}'"
@@ -36,15 +37,26 @@ module YARD
             Dir.chdir(spec.full_gem_path)
             log.info "Building yardoc index for gem: #{spec.full_name}"
             Yardoc.run('-n', '-b', yfile)
-            reload = false
           end
+        end
+      end
+      
+      def add_gems(gems)
+        gems.each_slice(2) do |gem, ver_require|
+          specs = Gem.source_index.find_name(gem, ver_require || ">= 0")
+          @gems += specs unless specs.empty?
         end
       end
       
       # Parses options
       def optparse(*args)
         opts = OptionParser.new
-        opts.on('--rebuild', 'Rebuilds indexes for all gems') do
+        opts.banner = 'Usage: yard gems [options] [gem_name [version]]'
+        opts.separator ""
+        opts.separator "#{description}. If no gem_name is given,"
+        opts.separator "all gems are built."
+        opts.separator ""
+        opts.on('--rebuild', 'Rebuilds index') do
           @rebuild = true
         end
         opts.on('--legacy', 'Use old style parser and handlers. Unavailable under Ruby 1.8.x') do
@@ -53,6 +65,7 @@ module YARD
         
         common_options(opts)
         parse_options(opts, args)
+        add_gems(args)
       end
     end
   end
