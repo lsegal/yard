1ac525818191b8fd1904e8c6bb9727a8540bd763
Loren Segal <lsegal@soen.ca>
2010-06-21 03:44:25 -0400

    Ignore private and @private classes/modules from alphabetic index

--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -41,6 +41,7 @@ end
 def index
   @objects_by_letter = {}
   objects = @objects.reject {|o| o.root? }.sort_by {|o| o.name.to_s }
+  objects = run_verifier(objects)
   objects.each {|o| (@objects_by_letter[o.name.to_s[0,1].upcase] ||= []) << o }
   erb(:index)
 end
