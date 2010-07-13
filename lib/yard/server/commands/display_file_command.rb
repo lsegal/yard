a47a418425ff6d045051c05f7e7f3fb6a6beab59
Loren Segal <lsegal@soen.ca>
2010-07-13 13:27:16 -0400

    Remove reference to MIME_TYPES

--- lib/yard/server/commands/display_file_command.rb
+++ lib/yard/server/commands/display_file_command.rb
@@ -7,7 +7,7 @@ module YARD
           filename = File.cleanpath(File.join(library.source_path, path))
           raise NotFoundError if !File.file?(filename)
           if filename =~ /\.(jpe?g|gif|png|bmp)$/i
-            headers['Content-Type'] = StaticFileCommand::MIME_TYPES[$1.downcase.to_sym] || 'text/html'
+            headers['Content-Type'] = StaticFileCommand::DefaultMimeTypes[$1.downcase] || 'text/html'
             render IO.read(filename)
           else
             options.update(:object => Registry.root, :type => :layout, :file => filename)
