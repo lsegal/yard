6a1bd22341809d2dacbefb3079ca12787e6bb930
Loren Segal <lsegal@soen.ca>
2010-07-13 15:28:26 -0400

    Clean up Commands::Base#cache specs

--- spec/server/commands/base_spec.rb
+++ spec/server/commands/base_spec.rb
@@ -4,30 +4,34 @@ class MyProcCommand < Base
   def initialize(&block) self.class.send(:define_method, :run, &block) end
 end
 
+class MyCacheCommand < Base
+  def run; cache 'foo' end
+end
+
 describe YARD::Server::Commands::Base do
-  before do
-    @adapter = mock_adapter
-    @command = Base.new(:adapter => @adapter, :caching => true, :request => OpenStruct.new)
-  end
-  
   describe '#cache' do
+    before do
+      @command = MyCacheCommand.new(:adapter => mock_adapter, :caching => true)
+      @command.request = OpenStruct.new
+    end
+
     it "should not cache if caching == false" do
       File.should_not_receive(:open)
       @command.caching = false
-      @command.send(:cache, "foo")
+      @command.run
     end
     
     it "should require document root to cache" do
       File.should_not_receive(:open)
-      @adapter.document_root = nil
-      @command.send(:cache, "foo")
+      @command.adapter.document_root = nil
+      @command.run
     end
     
     it "should cache to path/to/file.html and create directories" do
       FileUtils.should_receive(:mkdir_p).with('/public/path/to')
       File.should_receive(:open).with('/public/path/to/file.html', anything)
       @command.request.path = '/path/to/file.html'
-      @command.send(:cache, "foo")
+      @command.run
     end
   end
   
