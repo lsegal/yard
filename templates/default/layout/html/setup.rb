2e0359d07077a04fa22fd22049503ce4f32a92e2
Loren Segal <lsegal@soen.ca>
2010-08-28 03:14:46 -0400

    Fix bug in generating index page with --use-cache

--- templates/default/layout/html/setup.rb
+++ templates/default/layout/html/setup.rb
@@ -39,7 +39,7 @@ end
 
 def index
   @objects_by_letter = {}
-  objects = @objects.reject {|o| o.root? }.sort_by {|o| o.name.to_s }
+  objects = Registry.all(:class, :module).sort_by {|o| o.name.to_s }
   objects = run_verifier(objects)
   objects.each {|o| (@objects_by_letter[o.name.to_s[0,1].upcase] ||= []) << o }
   erb(:index)
