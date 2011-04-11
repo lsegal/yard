2ee42b9fff71032af8a13167083ddb3052d24561
Loren Segal <lsegal@soen.ca>
2011-04-11 05:33:06 -0400

    Fix specs due to changes in ExtraFileObject API

--- spec/cli/yardoc_spec.rb
+++ spec/cli/yardoc_spec.rb
@@ -390,8 +390,8 @@ describe YARD::CLI::Yardoc do
       Dir.should_receive(:glob).with('README*').and_return([])
       File.should_receive(:file?).with('extra_file1').and_return(true)
       File.should_receive(:file?).with('extra_file2').and_return(true)
-      File.should_receive(:readlines).with('extra_file1').and_return([''])
-      File.should_receive(:readlines).with('extra_file2').and_return([''])
+      File.should_receive(:read).with('extra_file1').and_return('')
+      File.should_receive(:read).with('extra_file2').and_return('')
       @yardoc.parse_arguments *%w( file1 file2 - extra_file1 extra_file2 )
       @yardoc.files.should == %w( file1 file2 )
       @yardoc.options[:files].should == 
@@ -408,8 +408,8 @@ describe YARD::CLI::Yardoc do
     it "should accept globs as extra files" do
       Dir.should_receive(:glob).with('README*').and_return []
       Dir.should_receive(:glob).with('*.txt').and_return ['a.txt', 'b.txt']
-      File.should_receive(:readlines).with('a.txt').and_return([''])
-      File.should_receive(:readlines).with('b.txt').and_return([''])
+      File.should_receive(:read).with('a.txt').and_return('')
+      File.should_receive(:read).with('b.txt').and_return('')
       File.should_receive(:file?).with('a.txt').and_return(true)
       File.should_receive(:file?).with('b.txt').and_return(true)
       @yardoc.parse_arguments *%w( file1 file2 - *.txt )
