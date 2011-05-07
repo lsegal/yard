62e8baab0a1e1783e714cb8ac525f1343c05a013
Loren Segal <lsegal@soen.ca>
2011-05-07 15:36:34 -0400

    Show instance attributes in method list for yard server
    Closes #302

--- lib/yard/server/templates/doc_server/full_list/html/setup.rb
+++ lib/yard/server/templates/doc_server/full_list/html/setup.rb
@@ -2,9 +2,19 @@ include T('default/fulldoc/html')
 
 def init
   case @list_type.to_sym
-  when :class; @list_title = "Class List"
-  when :methods; @list_title = "Method List"
-  when :files; @list_title = "File List"
+  when :class
+    generate_class_list
+    @list_title = "Class List"
+  when :methods
+    generate_method_list
+    @list_title = "Method List"
+  when :files
+    generate_file_list
+    @list_title = "File List"
   end
   sections :full_list
+end
+
+def asset(file, contents)
+  contents
 end
