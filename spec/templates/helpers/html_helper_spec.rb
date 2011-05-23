d3b753b1644b18667983213d917f2b99b9a43e37
Loren Segal <lsegal@soen.ca>
2011-05-23 12:36:29 -0400

    Load markup provider before stubbing file so specs pass

--- spec/templates/helpers/html_helper_spec.rb
+++ spec/templates/helpers/html_helper_spec.rb
@@ -118,6 +118,7 @@ describe YARD::Templates::Helpers::HtmlHelper do
     end
     
     it "should include file and htmlify it" do
+      load_markup_provider(:rdoc)
       File.should_receive(:file?).with('foo.rdoc').and_return(true)
       File.should_receive(:read).with('foo.rdoc').and_return('= HI')
       htmlify("{include:file:foo.rdoc}", :rdoc).gsub(/\s+/, '').should == "<p><h1>HI</h1></p>"
