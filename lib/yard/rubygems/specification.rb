f3a77ecb2c492c4439049bbf1ac63a00dd0d64d1
Loren Segal <lsegal@soen.ca>
2011-06-19 12:41:56 -0400

    Support RubyGems 1.8.x in rubygems_plugin.rb

--- lib/yard/rubygems/specification.rb
+++ lib/yard/rubygems/specification.rb
@@ -25,10 +25,10 @@ class Gem::Specification
     attr_accessor :has_rdoc
   end
 
-  if defined?(Gem::VERSION) && Gem::VERSION =~ /^1\.7\./
+  if defined?(Gem::VERSION) && Gem::VERSION >= '1.7.'
     def _dump_with_rdoc(limit)
       dmp = _dump_without_rdoc(limit)
-      dmp[15] = @has_rdoc
+      dmp[15] = @has_rdoc if dmp[15] == true
       dmp
     end
     alias _dump_without_rdoc _dump
