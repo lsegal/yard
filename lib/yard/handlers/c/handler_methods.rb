6cebf09875241d4770fe29b6f044239ef0290a87
Loren Segal <lsegal@soen.ca>
2012-01-08 03:46:10 -0500

    Move handle_alias into HandlerMethods

--- lib/yard/handlers/c/handler_methods.rb
+++ lib/yard/handlers/c/handler_methods.rb
@@ -46,6 +46,25 @@ module YARD
           end
         end
         
+        def handle_alias(var_name, new_name, old_name)
+          namespace = namespace_for_variable(var_name)
+          new_meth, old_meth = new_name.to_sym, old_name.to_sym
+          old_obj = namespace.child(:name => old_meth, :scope => :instance)
+          new_obj = register CodeObjects::MethodObject.new(namespace, new_meth, :instance) do |o|
+            o.visibility = visibility
+          end
+
+          if old_obj
+            new_obj.signature = old_obj.signature
+            new_obj.source = old_obj.source
+            new_obj.docstring = old_obj.docstring
+            new_obj.docstring.object = new_obj
+          else
+            new_obj.signature = "def #{new_meth}" # this is all we know.
+          end
+
+          namespace.aliases[new_obj] = old_meth
+        end
         
         private
 
