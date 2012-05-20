31254d7f5afcd053f87459ffaad955fd7f0dc3cf
Loren Segal <lsegal@soen.ca>
2012-05-19 20:55:57 -0400

    Check non-Init methods for object declarations in CRuby code
    Closes #543

--- spec/handlers/c/init_handler_spec.rb
+++ spec/handlers/c/init_handler_spec.rb
@@ -33,4 +33,15 @@ describe YARD::Handlers::C::InitHandler do
     eof
     Registry.at('A').docstring.should == 'Foo!'
   end
+
+  it "should check non-Init methods for declarations too" do
+    parse(<<-eof)
+      void foo(int x, int y, char *name) {
+        rb_cB = rb_define_class("B", rb_cObject);
+        rb_define_method(rb_cB, "foo", foo_impl, 0);
+      }
+    eof
+    Registry.at('B').should be_a(CodeObjects::ClassObject)
+    Registry.at('B#foo').should be_a(CodeObjects::MethodObject)
+  end
 end
