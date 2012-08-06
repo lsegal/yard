8406cd7ada637a0eda4c17429dd2e4caf3d06902
Loren Segal <lsegal@soen.ca>
2012-08-06 19:02:27 -0400

    Undef source_index safely

--- lib/yard/rubygems/backports/gem.rb
+++ lib/yard/rubygems/backports/gem.rb
@@ -1,6 +1,6 @@
 module Gem
   class << self
-    undef source_index
+    undef source_index if method_defined?(:source_index)
     # Returns the Gem::SourceIndex of specifications that are in the Gem.path
     def source_index
       @@source_index ||= SourceIndex.from_installed_gems
