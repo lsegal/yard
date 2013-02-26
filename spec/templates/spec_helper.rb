3cdaa2842d8b45975a212428313be2aa1e564f36
Loren Segal <lsegal@soen.ca>
2013-02-26 00:50:05 -0800

    Fix remaining text (changes in generated HTML characters)

--- spec/templates/spec_helper.rb
+++ spec/templates/spec_helper.rb
@@ -26,6 +26,7 @@ end
 def html_equals_string(result, expected)
   [expected, result].each do |value|
     value.gsub!(/(>)\s+|\s+(<)/, '\1\2')
+    value.gsub!(/&#39;/, "'")
     value.strip!
   end
   text_equals_string(result, expected)
