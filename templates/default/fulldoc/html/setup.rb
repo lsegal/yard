da686cc53c4a8ecb5cbf6e2bf2b70dbf914b1451
Trevor Rowe <trevorrowe@gmail.com>
2015-03-03 13:09:33 -0800

    Improved initial rendering of class list.
    The side `#full_list` that provides a tree of classes renders slowly
    and jumps after page load because much of its formatting is provided
    by JavaScript. Large trees suffer in render and it can be jarring when
    navigating the tree.
    
    To address the issue, this commit makes the following changes:
    
    * Updated the `class_list` method in the default template to apply
      the initial css classes necessary to expand the tree 1 level,
      alternate row colors, and to indent nested items.
    
    * The previous `class_list` method used ul elements as siblings to
      li elements and relied on padding to nest. The ul elements for
      nested classes are now children of the li elements. This
      improves the performance of initial rendering as JavaScript no
      longer must apply padding.
    
    * Improved search speed on large trees by removing the use of
      setTimeout and instead the tree is detached from the dom and
      re-attached after modifications are made to show search
      results. This prevents the browser from locking up during
      dom updates.
    
    Some other thoughts for improvements, not implemented by this
    pull-request:
    
      * There is still a notable delay in highlighting the current
        "clicked" section of the tree and expanding to it as the
        message for what element should be clicked is being sent
        from the parent window.  This message is currently being
        sent when the page completes. This message should be
        sent straightway after the dom loads, but before images
        and JavaScript has loaded, especially analytics or other
        possible things added to the default template. Alternatively,
        if the frame could detect the clicked element itself
        that could eliminate the delay.

--- templates/default/fulldoc/html/setup.rb
+++ templates/default/fulldoc/html/setup.rb
@@ -172,27 +172,70 @@ def generate_frameset
   asset(url_for_frameset, erb(:frames))
 end
 
+# @api private
+class TreeContext
+
+  def initialize
+    @depth = 0
+    @even_odd = Alternator.new(:even, :odd)
+  end
+
+  def nest
+    @depth += 1
+    yield
+    @depth -= 1
+  end
+
+  # @return [String] Returns a css pixel offset, e.g. "30px"
+  def indent
+    "#{(@depth + 2) * 15}px"
+  end
+
+  def classes
+    classes = []
+    classes << 'collapsed' if @depth > 0
+    classes << @even_odd.next if @depth < 2
+    classes
+  end
+
+  class Alternator
+    def initialize(first, second)
+      @next = first
+      @after = second
+    end
+    def next
+      @next, @after = @after, @next
+      @after
+    end
+  end
+
+end
+
 # @return [String] HTML output of the classes to be displayed in the
 #    full_list_class template.
-def class_list(root = Registry.root)
+def class_list(root = Registry.root, tree = TreeContext.new)
   out = ""
   children = run_verifier(root.children)
   if root == Registry.root
     children += @items.select {|o| o.namespace.is_a?(CodeObjects::Proxy) }
   end
-  children.reject {|c| c.nil? }.sort_by {|child| child.path }.map do |child|
+  children.compact.sort_by(&:path).each do |child|
     if child.is_a?(CodeObjects::NamespaceObject)
       name = child.namespace.is_a?(CodeObjects::Proxy) ? child.path : child.name
       has_children = run_verifier(child.children).any? {|o| o.is_a?(CodeObjects::NamespaceObject) }
-      out << "<li id='object_#{child.path}'>"
+      out << "<li id='object_#{child.path}' class='#{tree.classes.join(' ')}'>"
+      out << "<div class='item' style='padding-left:#{tree.indent}'>"
       out << "<a class='toggle'></a> " if has_children
       out << linkify(child, name)
       out << " &lt; #{child.superclass.name}" if child.is_a?(CodeObjects::ClassObject) && child.superclass
       out << "<small class='search_info'>"
       out << child.namespace.title
       out << "</small>"
+      out << "</div>"
+      tree.nest do
+        out << "<ul>#{class_list(child, tree)}</ul>" if has_children
+      end
       out << "</li>"
-      out << "<ul>#{class_list(child)}</ul>" if has_children
     end
   end
   out
