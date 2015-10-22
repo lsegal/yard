01015a46f83fcb353a1cf9e791cd6b078afc9e1c
Alex Dowad <alexinbeijing@gmail.com>
2015-10-22 19:38:42 +0200

    Update specs under spec/tags

--- spec/tags/library_spec.rb
+++ spec/tags/library_spec.rb
@@ -5,30 +5,30 @@ describe YARD::Tags::Library do
     Docstring.new(docstring).tags.first
   end
 
-  describe '#see_tag' do
-    it "should take a URL" do
-      tag("@see http://example.com").name.should == "http://example.com"
+  describe "#see_tag" do
+    it "takes a URL" do
+      expect(tag("@see http://example.com").name).to eq "http://example.com"
     end
 
-    it "should take an object path" do
-      tag("@see String#reverse").name.should == "String#reverse"
+    it "takes an object path" do
+      expect(tag("@see String#reverse").name).to eq "String#reverse"
     end
 
-    it "should take a description after the url/object" do
+    it "takes a description after the url/object" do
       tag = tag("@see http://example.com An Example Site")
-      tag.name.should == "http://example.com"
-      tag.text.should == "An Example Site"
+      expect(tag.name).to eq "http://example.com"
+      expect(tag.text).to eq "An Example Site"
     end
   end
 
-  describe '.define_tag' do
-    it "should allow defining tags with '.' in the name (x.y.z defines method x_y_z)" do
+  describe ".define_tag" do
+    it "allows defining tags with '.' in the name (x.y.z defines method x_y_z)" do
       Tags::Library.define_tag("foo", 'x.y.z')
       Tags::Library.define_tag("foo2", 'x.y.zz', Tags::OverloadTag)
-      Tags::Library.instance.method(:x_y_z_tag).should_not be_nil
-      Tags::Library.instance.method(:x_y_zz_tag).should_not be_nil
-      tag('@x.y.z foo bar').text.should == 'foo bar'
-      tag('@x.y.zz foo(bar)').signature.should == 'foo(bar)'
+      expect(Tags::Library.instance.method(:x_y_z_tag)).not_to be nil
+      expect(Tags::Library.instance.method(:x_y_zz_tag)).not_to be nil
+      expect(tag('@x.y.z foo bar').text).to eq 'foo bar'
+      expect(tag('@x.y.zz foo(bar)').signature).to eq 'foo(bar)'
     end
   end
 end
