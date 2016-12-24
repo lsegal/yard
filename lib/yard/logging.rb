e5e620249d4a6493a5d2cef6928d54283e56ceca
Ivo Anjo <ivo.anjo@ist.utl.pt>
2016-12-24 12:03:30 +0000

    Fix "warning: instance variable @progress_msg not initialized"

--- lib/yard/logging.rb
+++ lib/yard/logging.rb
@@ -48,6 +48,7 @@ module YARD
       self.formatter = method(:format_log)
       @progress_indicator = 0
       @mutex = Mutex.new
+      @progress_msg = nil
     end
 
     # Changes the debug level to DEBUG if $DEBUG is set
