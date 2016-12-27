18e5f844fae86ffcc46f96332a853def32de4958
Ivo Anjo <ivo.anjo@ist.utl.pt>
2016-12-27 08:35:04 +0000

    Enable printing of warnings during test suite execution
    This is still a bit noisy, but most of the noise is due to repeated
    warnings for the same things, rather than many different warnings, so
    let's enable this by default hopefully this will motivate someone to fix
    the remaining warnings :)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -179,7 +179,7 @@ RSpec.configure do |config|
 
   # This setting enables warnings. It's recommended, but in some cases may
   # be too noisy due to issues in dependencies.
-  #config.warnings = true # FIXME: Too noisy right now
+  config.warnings = true
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
