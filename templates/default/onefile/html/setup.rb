56998180b379c0161db180f943ae8101912cad7e
Loren Segal <lsegal@soen.ca>
2019-01-21 16:53:48 -0800

    Fix frozen string modification in onefile template
    Fixes #1184

--- templates/default/onefile/html/setup.rb
+++ templates/default/onefile/html/setup.rb
@@ -41,7 +41,7 @@ def parse_top_comments_from_file
   tokens = TokenList.new(@readme.contents)
   tokens.each do |token|
     break unless token.is_a?(RubyToken::TkCOMMENT) || token.is_a?(RubyToken::TkNL)
-    data << (token.text[/\A#\s{0,1}(.*)/, 1] || "\n")
+    data += (token.text[/\A#\s{0,1}(.*)/, 1] || "\n")
   end
   YARD::Docstring.new(data)
 end
