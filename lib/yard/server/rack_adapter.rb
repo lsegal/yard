4864dbe69d2af11d75122b8d10e06a422f3a8d85
Carlos Palhares <chjunior@gmail.com>
2025-02-24 23:07:27 -0300

    Use Rack::Server in Yard::Server::RackAdapter

--- lib/yard/server/rack_adapter.rb
+++ lib/yard/server/rack_adapter.rb
@@ -1,17 +1,9 @@
 # frozen_string_literal: true
 
+require 'rack'
+
 module YARD
   module Server
-    begin
-      require 'rackup'
-      # @private
-      RackServer = Rackup::Server
-    rescue LoadError
-      require 'rack'
-      # @private
-      RackServer = Rack::Server
-    end
-
     # This class wraps the {RackAdapter} into a Rack-compatible middleware.
     # See {#initialize} for a list of options to pass via Rack's +#use+ method.
     #
@@ -68,7 +60,7 @@ module YARD
       # block.
       # @return [void]
       def start
-        server = RackServer.new(server_options)
+        server = Rack::Server.new(server_options)
         server.instance_variable_set("@app", self)
         print_start_message(server)
         server.start
